d211f3916c239a41ac5c98105fb83ff101feae17
Manodeep Sinha <manodeep@gmail.com>
2014-12-29 08:04:59 -0600

    Included the right header files

--- xi_theory/xi_of_r/countpairs.h
+++ xi_theory/xi_of_r/countpairs.h
@@ -1,14 +1,17 @@
 #pragma once
 
+#include "function_precision.h" //for definition of DOUBLE
+#include <inttypes.h> //for uint64_t
+
 void countpairs(const int ND1, const DOUBLE * const X1, const DOUBLE * const Y1, const DOUBLE  * const Z1,
-				const int ND2, const DOUBLE * const X2, const DOUBLE * const Y2, const DOUBLE  * const Z2,
-				const DOUBLE xmin, const DOUBLE xmax,
-				const DOUBLE ymin, const DOUBLE ymax,
-				const DOUBLE zmin, const DOUBLE zmax,
-				const int autocorr,
-				const double rpmax,
+								const int ND2, const DOUBLE * const X2, const DOUBLE * const Y2, const DOUBLE  * const Z2,
+								const DOUBLE xmin, const DOUBLE xmax,
+								const DOUBLE ymin, const DOUBLE ymax,
+								const DOUBLE zmin, const DOUBLE zmax,
+								const int autocorr,
+								const double rpmax,
 #ifdef USE_OMP
-				const int numthreads,
+								const int numthreads,
 #endif
-				const int nrpbin, const double * restrict rupp);
+								const int nrpbin, const double * restrict rupp);
 
