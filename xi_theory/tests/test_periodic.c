de1212c9adda809cb5cc14ec1ba53e21b689d5aa
Manodeep Sinha <manodeep@users.noreply.github.com>
2015-01-24 08:48:58 -0600

    6 periodic tests are now working

--- xi_theory/tests/test_periodic.c
+++ xi_theory/tests/test_periodic.c
@@ -35,32 +35,39 @@
 
 char tmpoutputfile[]="./tmp_output.txt";
 
-int test_periodic_DD(void);
-int test_periodic_DDrppi(void);
-int test_wp(void);
+int test_periodic_DD(const char *correct_outputfile);
+int test_periodic_DDrppi(const char *correct_outputfile);
+int test_wp(const char *correct_outputfile);
+void read_data_and_set_globals(const char *firstfilename, const char *firstformat,const char *secondfilename,const char *secondformat);
 
 //Global variables
 int ND1;
 DOUBLE *X1=NULL,*Y1=NULL,*Z1=NULL;
 
+int ND2;
+DOUBLE *X2=NULL,*Y2=NULL,*Z2=NULL;
+
 char binfile[]="bins";
-const DOUBLE pimax=40.0;
+DOUBLE pimax=40.0;
 double boxsize=420.0;
-const int autocorr=1;
 #ifdef USE_OMP
 const int nthreads=4;
 #endif
+
+char current_file1[MAXLEN],current_file2[MAXLEN];
+
 //end of global variables
 
 
-int test_periodic_DD(void)
+int test_periodic_DD(const char *correct_outputfile)
 {
   struct timeval t0,t1; 
+  int autocorr = (X1==X2) ? 1:0;
 
   //Do the straight-up DD counts                                                                                                                                                                                                             
   gettimeofday(&t0,NULL);
   results_countpairs *results = countpairs(ND1,X1,Y1,Z1,
-										   ND1,X1,Y1,Z1,
+										   ND2,X2,Y2,Z2,
 #ifdef USE_OMP
 										   nthreads,
 #endif
@@ -79,7 +86,7 @@ int test_periodic_DD(void)
   fclose(fp);
 	
   char execstring[MAXLEN];
-  my_snprintf(execstring,MAXLEN,"diff -q Mr19_DD_periodic %s",tmpoutputfile);
+  my_snprintf(execstring,MAXLEN,"diff -q %s %s",correct_outputfile,tmpoutputfile);
   int ret=system(execstring);
   if(ret==EXIT_SUCCESS) {
 	fprintf(stderr,ANSI_COLOR_GREEN "PASSED: 3-D periodic DD calculation. Time taken = %8.2lf seconds " ANSI_COLOR_RESET "\n", pair_time);
@@ -91,13 +98,14 @@ int test_periodic_DD(void)
   return ret;
 }
 
-int test_periodic_DDrppi(void)
+int test_periodic_DDrppi(const char *correct_outputfile)
 {
   struct timeval t0,t1; 
   gettimeofday(&t0,NULL);
+  int autocorr = (X1==X2) ? 1:0;
   
   results_countpairs_rp_pi *results = countpairs_rp_pi(ND1,X1,Y1,Z1,
-													   ND1,X1,Y1,Z1,
+													   ND2,X2,Y2,Z2,
 #ifdef USE_OMP
 													   nthreads,
 #endif
@@ -119,7 +127,7 @@ int test_periodic_DDrppi(void)
   }
   fclose(fp);
   char execstring[MAXLEN];
-  my_snprintf(execstring,MAXLEN,"diff -q Mr19_DDrppi_periodic %s",tmpoutputfile);
+  my_snprintf(execstring,MAXLEN,"diff -q %s %s",correct_outputfile,tmpoutputfile);
   int ret=system(execstring);
   if(ret==EXIT_SUCCESS) {
 	fprintf(stderr,ANSI_COLOR_GREEN "PASSED: periodic DDrppi calculation. Time taken = %8.2lf seconds " ANSI_COLOR_RESET "\n", pair_time);
@@ -133,7 +141,7 @@ int test_periodic_DDrppi(void)
   return ret;
 }
 
-int test_wp(void)
+int test_wp(const char *correct_outputfile)
 {
   struct timeval t0,t1; 
 
@@ -155,7 +163,7 @@ int test_wp(void)
   }
   fclose(fp);
   char execstring[MAXLEN];
-  my_snprintf(execstring,MAXLEN,"diff -q Mr19_wp %s",tmpoutputfile);
+  my_snprintf(execstring,MAXLEN,"diff -q %s %s",correct_outputfile,tmpoutputfile);
   int ret=system(execstring);
   if(ret==EXIT_SUCCESS) {
 	fprintf(stderr,ANSI_COLOR_GREEN "PASSED: periodic wp calculation. Time taken = %8.2lf seconds " ANSI_COLOR_RESET "\n", pair_time);
@@ -168,29 +176,88 @@ int test_wp(void)
   return ret;
 }
 
+
+void read_data_and_set_globals(const char *firstfilename, const char *firstformat,const char *secondfilename,const char *secondformat)
+{
+
+  //Check to see if data has to be read for X1/Y1/Z1
+  if (strncmp(current_file1,firstfilename,strlen(current_file1)) != 0) {
+	//replace the data-set
+	if(X1 != NULL) {
+	  free(X1);free(Y1);free(Z1);
+	}
+	if(strncmp(current_file1,current_file2,strlen(current_file1)) == 0){
+	  //X2/Y2/Z2 were also pointing towards X1/Y1/Z1
+	  X2=NULL;Y2=NULL;Z2=NULL;
+	}
+	ND1 = read_positions(firstfilename,firstformat,(void **) &X1,(void **) &Y1,(void **) &Z1,sizeof(DOUBLE));
+	strlcpy(current_file1,firstfilename,MAXLEN);
+  }
+  
+  //first check if only one unique file is asked for
+  if(strncmp(firstfilename,secondfilename,strlen(firstfilename))==0) {
+	//But X2 might have read-in a different file->avoid a memory-leak
+	if(X2 != NULL && X2 != X1) {
+	  free(X2);free(Y2);free(Z2);
+	}
+	X2=X1;
+	Y2=Y1;
+	Z2=Z1;
+	ND2=ND1;
+	return;
+  }
+
+
+  //Check to see if data has to be read for X2/Y2/Z2
+  if (strncmp(current_file2,secondfilename,strlen(current_file2)) != 0) {
+	//replace the data-set
+	if(X2 != NULL && X2 != X1) {
+	  free(X2);free(Y2);free(Z2);
+	}
+	ND2 = read_positions(secondfilename,secondformat,(void **) &X2,(void **) &Y2,(void **) &Z2,sizeof(DOUBLE));
+	strlcpy(current_file2,secondfilename,MAXLEN);
+  }
+}
+
+
 int main(int argc, char **argv)
 {
   struct timeval t0,t1;
   char file[]="data/gals_Mr19.ff";
   char fileformat[]="f";
 
+  gettimeofday(&t0,NULL);
+
   //set the globals
   ND1 = read_positions(file,fileformat,(void **) &X1,(void **) &Y1,(void **) &Z1,sizeof(DOUBLE));
+  ND2 = ND1;
+  X2 = X1;
+  Y2 = Y1;
+  Z2 = Z1;
 
-  gettimeofday(&t0,NULL);
+  strlcpy(current_file1,file,MAXLEN);
+  strlcpy(current_file2,file,MAXLEN);
   
   int failed=0;
   int status;
   
-  const char allfunction_names[][MAXLEN] = {"tests_periodic_DD","tests_periodic_DDrppi","tests_wp"};
-  const int ntests = sizeof(allfunction_names)/(sizeof(char)*MAXLEN);
-  int (*allfunctions[]) (void) = {test_periodic_DD,test_periodic_DDrppi,test_wp};
+  const char alltests_names[][MAXLEN] = {"Mr19 DD","Mr19 DDrppi","Mr19 wp","CMASS DDrppi DD","CMASS DDrppi DR","CMASS DDrppi RR"};
+  const int ntests = sizeof(alltests_names)/(sizeof(char)*MAXLEN);
+  const char correct_outoutfiles[][MAXLEN] = {"Mr19_DD_periodic","Mr19_DDrppi_periodic","Mr19_wp","cmass_DD_periodic","cmass_DR_periodic","cmass_RR_periodic"};
+  const char firstfilename[][MAXLEN] = {"data/gals_Mr19.ff","data/gals_Mr19.ff","data/gals_Mr19.ff","data/cmassmock_Zspace.ff","data/cmassmock_Zspace.ff","data/random_Zspace.ff"};
+  const char firstfiletype[][MAXLEN] = {"f","f","f","f","f","f"};
+  const char secondfilename[][MAXLEN] = {"data/gals_Mr19.ff","data/gals_Mr19.ff","data/gals_Mr19.ff","data/cmassmock_Zspace.ff","data/random_Zspace.ff","data/random_Zspace.ff"};
+  const char secondfiletype[][MAXLEN] = {"f","f","f","f","f","f"};
+  const DOUBLE allpimax[]             = {40.0,40.0,40.0,80.0,80.0,80.0};
+  int (*allfunctions[]) (const char *) = {test_periodic_DD,test_periodic_DDrppi,test_wp,test_periodic_DDrppi,test_periodic_DDrppi,test_periodic_DDrppi};
   int total_tests=0;
   
   if(argc==1) {
 	//nothing was passed at the command-line run all tests
 	for(int i=0;i<ntests;i++) {
-	  status = (*allfunctions[i])();
+	  read_data_and_set_globals(firstfilename[i],firstfiletype[i],secondfilename[i],secondfiletype[i]);
+	  pimax=allpimax[i];
+	  status = (*allfunctions[i])(correct_outoutfiles[i]);
 	  total_tests++;
 	  if(status != EXIT_SUCCESS)  {
 		failed++;
@@ -202,7 +269,9 @@ int main(int argc, char **argv)
 	  int this_test_num = atoi(argv[i]);
 	  if(this_test_num >= 0 && this_test_num < ntests) {
 		total_tests++;
-		status = (*allfunctions[this_test_num])();
+		read_data_and_set_globals(firstfilename[this_test_num],firstfiletype[this_test_num],secondfilename[this_test_num],secondfiletype[this_test_num]);
+		pimax=allpimax[this_test_num];
+		status = (*allfunctions[this_test_num])(correct_outoutfiles[this_test_num]);
 		if(status != EXIT_SUCCESS)  {
 		  failed++;
 		}
