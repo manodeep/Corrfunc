7ff56626d2f8a94924049cec3890dc13a384fbf1
Manodeep Sinha <manodeep@users.noreply.github.com>
2015-01-23 18:09:10 -0600

    Added the files for the progressbar

---
+++ xi_theory/utils/progressbar.h
@@ -0,0 +1,20 @@
+#ifndef _PROGRESSBAR_H_
+#define _PROGRESSBAR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+
+#ifndef MAXLEN
+#define MAXLEN 1000
+#endif
+
+
+void init_my_progressbar(const int64_t N,int *interrupted);
+void my_progressbar(const int64_t curr_index,int *interrupted);
+void finish_myprogressbar(int *interrupted);
+
+
+#endif
