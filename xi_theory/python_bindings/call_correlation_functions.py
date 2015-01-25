f6f3b1ec44a165e536eac795f4d9122d9f882e74
Manodeep Sinha <manodeep@users.noreply.github.com>
2015-01-25 08:30:05 -0600

    Updated the python example routine

--- xi_theory/python_bindings/call_correlation_functions.py
+++ xi_theory/python_bindings/call_correlation_functions.py
@@ -11,27 +11,54 @@ import sys
 import numpy as np
 import _countpairs
 
-
 file="../tests/data/gals_Mr19.txt"
 ### Make sure the precision agrees with the definition in ../common.mk.
 ### Otherwise, you will get a runtime error -- 
 ### TypeError TypeError: array cannot be safely cast to required type
-dtype='float32' 
-x,y,z = np.loadtxt(file,dtype=dtype,unpack=True)
+dtype=np.float32
+x,y,z = np.genfromtxt(file,dtype=dtype,unpack=True)
 array = np.hstack((x,y,z))
 boxsize=np.max(array)
 nthreads=4
 pimax=40.0
 binfile="../tests/bins"
 autocorr=1
-results_wp = _countpairs.countpairs_wp(boxsize,pimax,nthreads,binfile,x,y,z)
-print("{}".format(results_wp))
-results_DDrppi = _countpairs.countpairs_rp_pi(autocorr,nthreads,pimax,binfile,x,y,z,x,y,z)
-print("{}".format(results_DDrppi))
-results_DD = _countpairs.countpairs(autocorr,nthreads,binfile,x,y,z,x,y,z)
-print("{}".format(results_DD))
+numbins_to_print=5
 
-        
+print("Running 3-D correlation function xi(r)")
+results_DD = _countpairs.countpairs(autocorr,nthreads,binfile,x,y,z,x,y,z)
+print("\n#      **** xi(r): first {} bins  *******       ".format(numbins_to_print))
+print("#      rmin        rmax       rpavg       npairs")
+print("################################################")
+for ibin in xrange(numbins_to_print):
+    items = results_DD[ibin]
+    print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10d}".format(items[0],items[1],items[2],items[3]))
+print("------------------------------------------------")
+
+
+print("\nRunning 2-D correlation function xi(rp,pi)")
+results_DDrppi = _countpairs.countpairs_rp_pi(autocorr,nthreads,pimax,binfile,x,y,z,x,y,z)
+print("\n#            ****** xi(rp,pi): first {} bins  *******      ".format(numbins_to_print))
+print("#      rmin        rmax       rpavg     pi_upper     npairs")
+print("###########################################################")
+for ibin in xrange(numbins_to_print):
+    items = results_DDrppi[ibin]
+    print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
+print("-----------------------------------------------------------")
+
+
+print("\nRunning 2-D projected correlation function wp(rp)")
+results_wp = _countpairs.countpairs_wp(boxsize,pimax,nthreads,binfile,x,y,z)
+print("\n#            ******    wp: first {} bins  *******         ".format(numbins_to_print))
+print("#      rmin        rmax       rpavg        wp       npairs")
+print("##########################################################")
+for ibin in xrange(numbins_to_print):
+    items = results_wp[ibin]
+    print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
+print("-----------------------------------------------------------")
+
+print("Done with all three correlation calculations.")
+
 
 
 
