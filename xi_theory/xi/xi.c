1e3559df40fcbad624b30d7463c16b1bfd633099
Manodeep Sinha <manodeep@users.noreply.github.com>
2015-01-28 18:21:09 -0600

    Quite a bit of directory restructuring and updating Makefiles. Added the xi sub-dir for computing full 3-D xi(r) on a periodic cube

--- xi_theory/xi/xi.c
+++ xi_theory/xi/xi.c
@@ -6,16 +6,15 @@
 		directory at https://bitbucket.org/manodeep/corrfunc/
 */
 
-/* PROGRAM wp
-   --- wp boxsize file format binfile pimax [Nthreads] > wpfile
-   --- Measure the projected auto-correlation function wp(rp) for a single periodic box
+/* PROGRAM xi
+   --- xi boxsize file format binfile [Nthreads] > xifile
+   --- Measure the auto-correlation function xi(r) for a single periodic box
 	   * boxsize      = BoxSize (in same units as X/Y/Z of the data)
-	   * file         = name of first data file
-	   * format       = format of first data file  (a=ascii, c=csv, f=fast-food)
+	   * file         = name of data file
+	   * format       = format of data file  (a=ascii, c=csv, f=fast-food)
 	   * binfile      = name of ascii file containing the r-bins (rmin rmax for each bin)
-	   * pimax        = maximum line-of-sight-separation
 	   * numthreads   = number of threads to use (only if USE_OMP is enabled)
-     > wpfile         = name of output file. Contains <wp [rpavg=0.0] rmin rmax npairs>
+     > xifile         = name of output file. Contains <xi [rpavg=0.0] rmin rmax npairs>
       ----------------------------------------------------------------------
 */
 
@@ -27,7 +26,7 @@
 
 #include "defs.h" //for ADD_DIFF_TIME
 #include "function_precision.h" //definition of DOUBLE
-#include "countpairs_wp.h" //function proto-type for countpairs
+#include "countpairs_xi.h" //function proto-type for countpairs
 #include "io.h" //function proto-type for file input
 #include "utils.h" //general utilities
 
@@ -39,10 +38,9 @@ int main(int argc, char *argv[])
 {
 
   /*---Arguments-------------------------*/
-	double boxsize;
-	char *file=NULL,*fileformat=NULL;
-	char *binfile=NULL;
-  DOUBLE pimax ;
+  double boxsize;
+  char *file=NULL,*fileformat=NULL;
+  char *binfile=NULL;
 	
   /*---Data-variables--------------------*/
   int64_t ND1=0;
@@ -51,10 +49,10 @@ int main(int argc, char *argv[])
 
   /*---Corrfunc-variables----------------*/
 #ifndef USE_OMP
-	const char argnames[][30]={"boxsize","file","format","binfile","pimax"};
+	const char argnames[][30]={"boxsize","file","format","binfile"};
 #else
 	int nthreads=2;
-	const char argnames[][30]={"boxsize","file","format","binfile","pimax","Nthreads"};
+	const char argnames[][30]={"boxsize","file","format","binfile","Nthreads"};
 #endif
   int nargs=sizeof(argnames)/(sizeof(char)*30);
   
@@ -80,22 +78,13 @@ int main(int argc, char *argv[])
     }
     return EXIT_FAILURE;
   }
-	boxsize=atof(argv[1]);
+  boxsize=atof(argv[1]);
   file=argv[2];
   fileformat=argv[3];
-	binfile=argv[4];
-	
-	pimax=40.0;
-
-#ifdef DOUBLE_PREC
-	sscanf(argv[5],"%lf",&pimax) ;
-#else    
-	sscanf(argv[5],"%f",&pimax) ;
-#endif    
-		
-		
+  binfile=argv[4];
+  
 #ifdef USE_OMP
-	nthreads=atoi(argv[6]);
+	nthreads=atoi(argv[5]);
 	assert(nthreads >= 1 && "Number of threads must be at least 1");
 #endif
 	
@@ -120,39 +109,38 @@ int main(int argc, char *argv[])
 	//check that theee positions are within limits
   for(int i=0;i<ND1;i++) {
     assert(x1[i] >= 0.0 && x1[i] <= boxsize && "xpos is within limits [0, boxsize]");
-		assert(y1[i] >= 0.0 && y1[i] <= boxsize && "ypos is within limits [0, boxsize]");
-		assert(z1[i] >= 0.0 && z1[i] <= boxsize && "zpos is within limits [0, boxsize]");
+	assert(y1[i] >= 0.0 && y1[i] <= boxsize && "ypos is within limits [0, boxsize]");
+	assert(z1[i] >= 0.0 && z1[i] <= boxsize && "zpos is within limits [0, boxsize]");
   }
 
   /*---Count-pairs--------------------------------------*/
   gettimeofday(&t0,NULL);
-	results_countpairs_wp *results = countpairs_wp(ND1, x1, y1, z1,
-																								 boxsize, 
+  results_countpairs_xi *results = countpairs_xi(ND1, x1, y1, z1,
+												   boxsize, 
 #ifdef USE_OMP
-																								 nthreads,
+												   nthreads,
 #endif
-																								 binfile,
-																								 pimax);
+												   binfile);
 
-	gettimeofday(&t1,NULL);
+  gettimeofday(&t1,NULL);
   double pair_time = ADD_DIFF_TIME(t0,t1);
-	free(x1);free(y1);free(z1);
-	
+  free(x1);free(y1);free(z1);
+  
 	//Output the results
 	/* Note: we discard the first bin, to mimic the fact that close pairs
 	 * are disregarded in SDSS data.
 	 */
 	DOUBLE rlow=results->rupp[0];
 	for(int i=1;i<results->nbin;++i) {
-		fprintf(stdout,"%e\t%e\t%e\t%e\t%12"PRIu64" \n",results->wp[i],results->rpavg[i],rlow,results->rupp[i],results->npairs[i]);
+		fprintf(stdout,"%e\t%e\t%e\t%e\t%12"PRIu64" \n",results->xi[i],results->rpavg[i],rlow,results->rupp[i],results->npairs[i]);
 		rlow=results->rupp[i];
 	}
 
 	//free the memory in the results struct
-	free_results_wp(&results);
+	free_results_xi(&results);
 	
   gettimeofday(&t_end,NULL);
-  fprintf(stderr,"wp> Done -  ND1=%12"PRId64". Time taken = %6.2lf seconds. read-in time = %6.2lf seconds pair-counting time = %6.2lf sec\n",
+  fprintf(stderr,"xi> Done -  ND1=%12"PRId64". Time taken = %6.2lf seconds. read-in time = %6.2lf seconds pair-counting time = %6.2lf sec\n",
 					ND1,ADD_DIFF_TIME(t_start,t_end),read_time,pair_time);
   return EXIT_SUCCESS;
 }
@@ -161,21 +149,20 @@ int main(int argc, char *argv[])
 void Printhelp(void)
 {
   fprintf(stderr,"=========================================================================\n") ;
-  fprintf(stderr,"   --- wp boxsize file format binfile pimax [Nthreads] > wpfile\n") ;
-  fprintf(stderr,"   --- Measure the projected auto-correlation function wp(rp) for a single periodic box\n") ;
+  fprintf(stderr,"   --- xi boxsize file format binfile [Nthreads] > xifile\n") ;
+  fprintf(stderr,"   --- Measure the auto-correlation function xi(r) for a single periodic box\n") ;
   fprintf(stderr,"     * boxsize      = BoxSize (in same units as X/Y/Z of the data)\n") ;
   fprintf(stderr,"     * file         = name of data file\n") ;
   fprintf(stderr,"     * format       = format of data file  (a=ascii, c=csv, f=fast-food)\n") ;
-  fprintf(stderr,"     * binfile       = name of ascii file containing the r-bins (rmin rmax for each bin)\n") ;
-  fprintf(stderr,"     * pimax         = maximum line-of-sight-separation\n") ;
+  fprintf(stderr,"     * binfile      = name of ascii file containing the r-bins (rmin rmax for each bin)\n") ;
 #ifdef USE_OMP
 	fprintf(stderr,"     * numthreads    = number of threads to use\n");
 #endif
 
 #ifdef OUTPUT_RPAVG	
-  fprintf(stderr,"     > wpfile        = name of output file. Contains <wp  rpavg  rmin rmax npairs>\n") ;
+  fprintf(stderr,"     > xifile        = name of output file. Contains <xi  rpavg  rmin rmax npairs>\n") ;
 #else
-  fprintf(stderr,"     > wpfile        = name of output file. Contains <wp [rpavg=0.0] rmin rmax npairs>\n") ;
+	fprintf(stderr,"     > xifile        = name of output file. Contains <xi [rpavg=0.0] rmin rmax npairs>\n") ;
 #endif	
 
 	fprintf(stderr,"\n\tCompile options: \n");
