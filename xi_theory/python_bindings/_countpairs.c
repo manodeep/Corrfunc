0fd35707a377519d8c0a54d64692677903792ae5
Manodeep Sinha <manodeep@users.noreply.github.com>
2015-02-09 17:54:48 -0600

    Added python binding for xi and vpf under xi_theory. Also, added to the exampels

--- xi_theory/python_bindings/_countpairs.c
+++ xi_theory/python_bindings/_countpairs.c
@@ -12,23 +12,34 @@
 #include "countpairs.h"
 #include "countpairs_rp_pi.h"
 #include "countpairs_wp.h"
+#include "countpairs_xi.h"
+
+//for the vpf
+#include "countspheres.h"
 
 //Docstrings for the methods
 static char module_docstring[]             =	"This module provides an interface for calculating correlation functions using C.";
 static char countpairs_docstring[]         =	"Calculate the 3-D \\xi auto/cross-correlation function given two sets of X1/Y1/Z1 and X2/Y2/Z2 arrays.";
 static char countpairs_rp_pi_docstring[]   =	"Calculate the 2-D DD(rp,pi) auto/cross-correlation function given two sets of X1/Y1/Z1 and X2/Y2/Z2 arrays.";
 static char countpairs_wp_docstring[]      =	"Calculate the projected auto-correlation function wp (assumes PERIODIC) given one set of X1/Y1/Z1 arrays.";
+static char countpairs_xi_docstring[]      =	"Calculate the 3-d auto-correlation function xi (assumes PERIODIC) given one set of X1/Y1/Z1 arrays.";
+static char countpairs_vpf_docstring[]     =	"Calculate the counts-in-spheres given one set of X1/Y1/Z1 arrays.";
 
 /* function proto-type*/
 static PyObject *countpairs_countpairs(PyObject *self, PyObject *args);
 static PyObject *countpairs_countpairs_rp_pi(PyObject *self, PyObject *args);
 static PyObject *countpairs_countpairs_wp(PyObject *self, PyObject *args);
+static PyObject *countpairs_countpairs_xi(PyObject *self, PyObject *args);
+static PyObject *countpairs_countpairs_vpf(PyObject *self, PyObject *args);
+
 PyMODINIT_FUNC init_countpairs(void);
 
 static PyMethodDef module_methods[] = {
 	{"countpairs"       ,countpairs_countpairs       ,METH_VARARGS,countpairs_docstring},
 	{"countpairs_rp_pi" ,countpairs_countpairs_rp_pi ,METH_VARARGS,countpairs_rp_pi_docstring},
 	{"countpairs_wp"    ,countpairs_countpairs_wp    ,METH_VARARGS,countpairs_wp_docstring},
+	{"countpairs_xi"    ,countpairs_countpairs_xi    ,METH_VARARGS,countpairs_xi_docstring},
+	{"countpairs_vpf"   ,countpairs_countpairs_vpf   ,METH_VARARGS,countpairs_vpf_docstring},
 	{NULL, NULL, 0, NULL}
 };
 
@@ -314,5 +325,154 @@ static PyObject *countpairs_countpairs_wp(PyObject *self, PyObject *args)
 		rlow=results->rupp[i];
 	}
 	free_results_wp(&results);
+	return ret;
+}
+
+
+static PyObject *countpairs_countpairs_xi(PyObject *self, PyObject *args)
+{
+	(void) self;//to suppress the unused variable warning. Terrible hack
+	PyObject *x1_obj, *y1_obj, *z1_obj;
+	double boxsize;
+	int nthreads=4;
+	char *binfile;
+	
+	if (!PyArg_ParseTuple(args, "disOOO",&boxsize,&nthreads,&binfile,&x1_obj,&y1_obj,&z1_obj))
+		return NULL;
+
+	/* Interpret the input objects as numpy arrays. */
+#ifdef DOUBLE_PREC	
+	PyObject *x1_array = PyArray_FROM_OTF(x1_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+	PyObject *y1_array = PyArray_FROM_OTF(y1_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+	PyObject *z1_array = PyArray_FROM_OTF(z1_obj, NPY_DOUBLE,	NPY_IN_ARRAY);
+#else
+	PyObject *x1_array = PyArray_FROM_OTF(x1_obj, NPY_FLOAT, NPY_IN_ARRAY);
+	PyObject *y1_array = PyArray_FROM_OTF(y1_obj, NPY_FLOAT, NPY_IN_ARRAY);
+	PyObject *z1_array = PyArray_FROM_OTF(z1_obj, NPY_FLOAT, NPY_IN_ARRAY);
+#endif
+	
+	if (x1_array == NULL || y1_array == NULL || z1_array == NULL) {
+		Py_XDECREF(x1_array);
+		Py_XDECREF(y1_array);
+		Py_XDECREF(z1_array);
+		return NULL;
+	}
+
+
+  /* How many data points are there? */
+	const int64_t ND1 = (int64_t)PyArray_DIM(x1_array, 0);
+
+	/* Get pointers to the data as C-types. */
+	DOUBLE *X1 = (DOUBLE *)PyArray_DATA(x1_array);
+	DOUBLE *Y1 = (DOUBLE *)PyArray_DATA(y1_array);
+	DOUBLE *Z1 = (DOUBLE *)PyArray_DATA(z1_array);
+	results_countpairs_xi *results = countpairs_xi(ND1,X1,Y1,Z1,
+																								 boxsize,
+#ifdef USE_OMP
+																								 nthreads,
+#endif
+																								 binfile);
+	
+	/* Clean up. */
+	Py_DECREF(x1_array);Py_DECREF(y1_array);Py_DECREF(z1_array);
+/* 	for(int i=1;i<results->nbin;i++) { */
+/* 		const DOUBLE rpavg = results->rpavg[i]; */
+/* 		fprintf(stderr,"%lf %lf %lf %lf %"PRIu64"\n",results->rupp[i-1],results->rupp[i],rpavg,results->xi[i],results->npairs[i]); */
+/* 	} */
+
+	
+	/* Build the output list */
+	PyObject *ret = PyList_New(0);
+	DOUBLE rlow=results->rupp[0];
+	for(int i=1;i<results->nbin;i++) {
+		PyObject *item = NULL;
+		const DOUBLE rpavg = results->rpavg[i];
+
+#ifdef DOUBLE_PREC		
+	  item = Py_BuildValue("(ddddk)", rlow,results->rupp[i],rpavg,results->xi[i],results->npairs[i]);
+#else
+		item = Py_BuildValue("(ffffk)", rlow,results->rupp[i],rpavg,results->xi[i],results->npairs[i]);
+#endif//DOUBLE_PREC
+
+		PyList_Append(ret, item);
+		Py_XDECREF(item);
+		rlow=results->rupp[i];
+	}
+	free_results_xi(&results);
+	return ret;
+}
+
+static PyObject *countpairs_countpairs_vpf(PyObject *self, PyObject *args)
+{
+	(void) self;//to suppress the unused variable warning. Terrible hack
+	PyObject *x1_obj, *y1_obj, *z1_obj;
+	double rmax;
+	int nbin,nc,num_pN;
+	unsigned long seed=-1;
+
+	
+	if (!PyArg_ParseTuple(args, "diiikOOO",&rmax,&nbin,&nc,&num_pN,&seed,&x1_obj,&y1_obj,&z1_obj))
+		return NULL;
+
+	/* Interpret the input objects as numpy arrays. */
+#ifdef DOUBLE_PREC	
+	PyObject *x1_array = PyArray_FROM_OTF(x1_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+	PyObject *y1_array = PyArray_FROM_OTF(y1_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+	PyObject *z1_array = PyArray_FROM_OTF(z1_obj, NPY_DOUBLE,	NPY_IN_ARRAY);
+#else
+	PyObject *x1_array = PyArray_FROM_OTF(x1_obj, NPY_FLOAT, NPY_IN_ARRAY);
+	PyObject *y1_array = PyArray_FROM_OTF(y1_obj, NPY_FLOAT, NPY_IN_ARRAY);
+	PyObject *z1_array = PyArray_FROM_OTF(z1_obj, NPY_FLOAT, NPY_IN_ARRAY);
+#endif
+	
+	if (x1_array == NULL || y1_array == NULL || z1_array == NULL) {
+		Py_XDECREF(x1_array);
+		Py_XDECREF(y1_array);
+		Py_XDECREF(z1_array);
+		return NULL;
+	}
+
+
+  /* How many data points are there? */
+	const int64_t ND1 = (int64_t)PyArray_DIM(x1_array, 0);
+
+	/* Get pointers to the data as C-types. */
+	DOUBLE *X1 = (DOUBLE *)PyArray_DATA(x1_array);
+	DOUBLE *Y1 = (DOUBLE *)PyArray_DATA(y1_array);
+	DOUBLE *Z1 = (DOUBLE *)PyArray_DATA(z1_array);
+
+	/* Do the VPF calculation */
+	results_countspheres *results = countspheres(ND1, X1, Y1, Z1,
+																							 rmax, nbin, nc,
+																							 num_pN,
+																							 seed);
+
+  /* Clean up. */
+	Py_DECREF(x1_array);Py_DECREF(y1_array);Py_DECREF(z1_array);
+
+	
+	/* Build the output list */
+	PyObject *ret = PyList_New(0);
+	const DOUBLE rstep = rmax/(DOUBLE)nbin ;
+	for(int ibin=0;ibin<results->nbin;ibin++) {
+		const double r=(ibin+1)*rstep;
+		PyObject *item = PyList_New(0);
+		PyObject *this_val = Py_BuildValue("d",r);
+		PyList_Append(item, this_val);
+		Py_XDECREF(this_val);
+		for(int i=0;i<num_pN;i++) {
+#ifdef DOUBLE_PREC					
+			this_val = Py_BuildValue("d",(results->pN)[ibin][i]);
+#else
+			this_val = Py_BuildValue("d",(results->pN)[ibin][i]);
+#endif
+			PyList_Append(item, this_val);
+			Py_XDECREF(this_val);
+		}
+		PyList_Append(ret, item);
+		Py_XDECREF(item);
+	}
+	
+	free_results_countspheres(&results);
 	return ret;
 }
