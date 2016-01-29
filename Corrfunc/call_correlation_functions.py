e8603c2c3174e159d39f7c71b4fa475d048573e7
Manodeep Sinha <manodeep@gmail.com>
2016-01-29 15:14:49 +1100

    Finally managed to add package data to the python package. Now, the C static libraries + headers also get installed into the python directory, along with the required catalogs + associated tests files

--- Corrfunc/call_correlation_functions.py
+++ Corrfunc/call_correlation_functions.py
@@ -22,122 +22,126 @@ except ImportError:
     pd = None
 from Corrfunc import _countpairs, rd
 
-tstart=time.time()
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_theory/tests/data/","gals_Mr19.txt")
-## Figure out the datatype, use the header file in the include directory
-## because that is most likely correct (common.mk might have been modified
-## but not recompiled)
-include_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                            "../include/", "countpairs.h")
-includes = rd(include_file)
-vector_type = re.search(r'(\w+)\s*\*\s*rupp\s*\;', includes, re.I).group(1)
-allowed_types = {"float":np.float32,"double":np.float}
-if vector_type not in list(allowed_types.keys()):
-    print("Error: Unknown precision={} found in header file {}. Allowed types are `{}'".format(vector_type,include_file,allowed_types))
-    sys.exit()
 
-dtype = allowed_types[vector_type]
+def main():
+    tstart=time.time()
+    file = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_theory/tests/data/","gals_Mr19.txt")
+    ## Figure out the datatype, use the header file in the include directory
+    ## because that is most likely correct (common.mk might have been modified
+    ## but not recompiled)
+    include_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                "../include/", "countpairs.h")
+    includes = rd(include_file)
+    vector_type = re.search(r'(\w+)\s*\*\s*rupp\s*\;', includes, re.I).group(1)
+    allowed_types = {"float":np.float32,"double":np.float}
+    if vector_type not in list(allowed_types.keys()):
+        print("Error: Unknown precision={} found in header file {}. Allowed types are `{}'".format(vector_type,include_file,allowed_types))
+        sys.exit()
 
-### check if pandas is available - much faster to read in the data through pandas
-t0=time.time()
-print("Reading in the data...")
-try:
-    if pd is not None:
-        df  = pd.read_csv(file,header=None,engine="c",dtype={"x":dtype,"y":dtype,"z":dtype},delim_whitespace=True)
-        x  = np.asarray(df[0],dtype=dtype)
-        y = np.asarray(df[1],dtype=dtype)
-        z  = np.asarray(df[2],dtype=dtype)
-    else:
-        x,y,z = np.genfromtxt(file,dtype=dtype,unpack=True)
-except:
-    pass
+    dtype = allowed_types[vector_type]
 
-t1=time.time()    
-print("Done reading the data - time taken = {0:10.1f} seconds.\nBeginning Correlation functions calculations".format(t1-t0))
+    ### check if pandas is available - much faster to read in the data through pandas
+    t0=time.time()
+    print("Reading in the data...")
+    try:
+        if pd is not None:
+            df  = pd.read_csv(file,header=None,engine="c",dtype={"x":dtype,"y":dtype,"z":dtype},delim_whitespace=True)
+            x  = np.asarray(df[0],dtype=dtype)
+            y = np.asarray(df[1],dtype=dtype)
+            z  = np.asarray(df[2],dtype=dtype)
+        else:
+            x,y,z = np.genfromtxt(file,dtype=dtype,unpack=True)
+    except:
+        pass
 
-boxsize=420.0
-nthreads=4
-pimax=40.0
-binfile=os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_theory/tests/","bins")
-autocorr=1
-numbins_to_print=5
+    t1=time.time()    
+    print("Done reading the data - time taken = {0:10.1f} seconds.\nBeginning Correlation functions calculations".format(t1-t0))
 
-print("Running 3-D correlation function DD(r)")
-results_DD = _countpairs.countpairs(autocorr,nthreads,binfile,x,y,z,x,y,z)
-print("\n#      **** DD(r): first {} bins  *******       ".format(numbins_to_print))
-print("#      rmin        rmax       rpavg       npairs")
-print("################################################")
-for ibin in range(numbins_to_print):
-    items = results_DD[ibin]
-    print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10d}".format(items[0],items[1],items[2],items[3]))
-print("------------------------------------------------")
+    boxsize=420.0
+    nthreads=4
+    pimax=40.0
+    binfile=os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_theory/tests/","bins")
+    autocorr=1
+    numbins_to_print=5
 
-
-print("\nRunning 2-D correlation function DD(rp,pi)")
-results_DDrppi = _countpairs.countpairs_rp_pi(autocorr,nthreads,pimax,binfile,x,y,z,x,y,z)
-print("\n#            ****** DD(rp,pi): first {} bins  *******      ".format(numbins_to_print))
-print("#      rmin        rmax       rpavg     pi_upper     npairs")
-print("###########################################################")
-for ibin in range(numbins_to_print):
-    items = results_DDrppi[ibin]
-    print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
-print("-----------------------------------------------------------")
+    print("Running 3-D correlation function DD(r)")
+    results_DD = _countpairs.countpairs(autocorr,nthreads,binfile,x,y,z,x,y,z)
+    print("\n#      **** DD(r): first {} bins  *******       ".format(numbins_to_print))
+    print("#      rmin        rmax       rpavg       npairs")
+    print("################################################")
+    for ibin in range(numbins_to_print):
+        items = results_DD[ibin]
+        print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10d}".format(items[0],items[1],items[2],items[3]))
+    print("------------------------------------------------")
 
 
-print("\nRunning 2-D projected correlation function wp(rp)")
-results_wp = _countpairs.countpairs_wp(boxsize,pimax,nthreads,binfile,x,y,z)
-print("\n#            ******    wp: first {} bins  *******         ".format(numbins_to_print))
-print("#      rmin        rmax       rpavg        wp       npairs")
-print("##########################################################")
-for ibin in range(numbins_to_print):
-    items = results_wp[ibin]
-    print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
-print("-----------------------------------------------------------")
-
-print("\nRunning 3-D auto-correlation function xi(r)")
-results_xi = _countpairs.countpairs_xi(boxsize,nthreads,binfile,x,y,z)
-print("\n#            ******    xi: first {} bins  *******         ".format(numbins_to_print))
-print("#      rmin        rmax       rpavg        xi       npairs")
-print("##########################################################")
-for ibin in range(numbins_to_print):
-    items = results_xi[ibin]
-    print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
-print("-----------------------------------------------------------")
-
-print("Done with all four correlation calculations.")
-
-print("\nRunning VPF pN(r)")
-rmax=10.0
-nbin=10
-nspheres=10000
-num_pN=3
-seed=-1
-results_vpf = _countpairs.countspheres_vpf(rmax,nbin,nspheres,num_pN,seed,x,y,z)
-
-print("\n#            ******    pN: first {} bins  *******         ".format(numbins_to_print))
-print('#       r    ',end="")
-
-for ipn in range(num_pN):
-    print('        p{:0d}      '.format(ipn),end="")
-
-print("")
-
-print("###########",end="")
-for ipn in range(num_pN):
-    print('################',end="")
-print("")
+    print("\nRunning 2-D correlation function DD(rp,pi)")
+    results_DDrppi = _countpairs.countpairs_rp_pi(autocorr,nthreads,pimax,binfile,x,y,z,x,y,z)
+    print("\n#            ****** DD(rp,pi): first {} bins  *******      ".format(numbins_to_print))
+    print("#      rmin        rmax       rpavg     pi_upper     npairs")
+    print("###########################################################")
+    for ibin in range(numbins_to_print):
+        items = results_DDrppi[ibin]
+        print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
+    print("-----------------------------------------------------------")
 
 
-for ibin in range(numbins_to_print):
-    items = results_vpf[ibin]
-    print('{0:10.2f} '.format(items[0]),end="")
+    print("\nRunning 2-D projected correlation function wp(rp)")
+    results_wp = _countpairs.countpairs_wp(boxsize,pimax,nthreads,binfile,x,y,z)
+    print("\n#            ******    wp: first {} bins  *******         ".format(numbins_to_print))
+    print("#      rmin        rmax       rpavg        wp       npairs")
+    print("##########################################################")
+    for ibin in range(numbins_to_print):
+        items = results_wp[ibin]
+        print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
+    print("-----------------------------------------------------------")
+
+    print("\nRunning 3-D auto-correlation function xi(r)")
+    results_xi = _countpairs.countpairs_xi(boxsize,nthreads,binfile,x,y,z)
+    print("\n#            ******    xi: first {} bins  *******         ".format(numbins_to_print))
+    print("#      rmin        rmax       rpavg        xi       npairs")
+    print("##########################################################")
+    for ibin in range(numbins_to_print):
+        items = results_xi[ibin]
+        print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
+    print("-----------------------------------------------------------")
+
+    print("Done with all four correlation calculations.")
+
+    print("\nRunning VPF pN(r)")
+    rmax=10.0
+    nbin=10
+    nspheres=10000
+    num_pN=3
+    seed=-1
+    results_vpf = _countpairs.countspheres_vpf(rmax,nbin,nspheres,num_pN,seed,x,y,z)
+
+    print("\n#            ******    pN: first {} bins  *******         ".format(numbins_to_print))
+    print('#       r    ',end="")
+
     for ipn in range(num_pN):
-        print(' {0:15.4e}'.format(items[ipn+1]),end="")
+        print('        p{:0d}      '.format(ipn),end="")
+
     print("")
 
-print("-----------------------------------------------------------")
+    print("###########",end="")
+    for ipn in range(num_pN):
+        print('################',end="")
+    print("")
 
-tend=time.time()
-print("Done with all functions. Total time taken = {0:10.1f} seconds. Read-in time = {1:10.1f} seconds.".format(tend-tstart,t1-t0))
 
+    for ibin in range(numbins_to_print):
+        items = results_vpf[ibin]
+        print('{0:10.2f} '.format(items[0]),end="")
+        for ipn in range(num_pN):
+            print(' {0:15.4e}'.format(items[ipn+1]),end="")
+        print("")
+
+    print("-----------------------------------------------------------")
+
+    tend=time.time()
+    print("Done with all functions. Total time taken = {0:10.1f} seconds. Read-in time = {1:10.1f} seconds.".format(tend-tstart,t1-t0))
+
+if __name__ == "__main__":
+    main()
 
--- Corrfunc/call_correlation_functions_mocks.py
+++ Corrfunc/call_correlation_functions_mocks.py
@@ -24,94 +24,122 @@ except ImportError:
 from Corrfunc import _countpairs_mocks, rd
 
 
-tstart=time.time()
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_mocks/tests/data/","Mr19_mock_northonly.rdcz.dat")
+def main():
+
+    tstart=time.time()
+    file = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_mocks/tests/data/","Mr19_mock_northonly.rdcz.dat")
 
 
-### The following section to figure out data-types seems to be no longer
-### required. 
+    ### The following section to figure out data-types seems to be no longer
+    ### required. 
 
 
-## Figure out the datatype, use the header file in the include directory
-## because that is most likely correct (common.mk might have been modified
-## but not recompiled)
-include_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                            "../include/", "countpairs_rp_pi_mocks.h")
-includes = rd(include_file)
-vector_type = re.search(r'(\w+)\s*\*\s*rupp\s*\;', includes, re.I).group(1)
-allowed_types = {"float":np.float32,"double":np.float}
-if vector_type not in list(allowed_types.keys()):
-    raise TypeError("Error: Unknown precision={} found in header file {}. Allowed types are `{}'".format(vector_type,include_file,allowed_types))
+    ## Figure out the datatype, use the header file in the include directory
+    ## because that is most likely correct (common.mk might have been modified
+    ## but not recompiled)
+    include_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                "../include/", "countpairs_rp_pi_mocks.h")
+    includes = rd(include_file)
+    vector_type = re.search(r'(\w+)\s*\*\s*rupp\s*\;', includes, re.I).group(1)
+    allowed_types = {"float":np.float32,"double":np.float}
+    if vector_type not in list(allowed_types.keys()):
+        raise TypeError("Error: Unknown precision={} found in header file {}. Allowed types are `{}'".format(vector_type,include_file,allowed_types))
 
-dtype = allowed_types[vector_type]
-dtype = np.float32
-### Check if pandas is available - much faster to read in the data through pandas
-t0=time.time()
-print("Reading in the data...")
-try:
-    if pd is not None:
-        df  = pd.read_csv(file,header=None,engine="c",dtype={"x":dtype,"y":dtype,"z":dtype},delim_whitespace=True)
-        ra  = np.asarray(df[0],dtype=dtype)
-        dec = np.asarray(df[1],dtype=dtype)
-        cz  = np.asarray(df[2],dtype=dtype)
-    else:
-        ra,dec,cz = np.genfromtxt(file,dtype=dtype,unpack=True)
-except:
-    pass
-
-        
-t1=time.time()    
-print("Done reading the data - time taken = {0:10.1f} seconds.\nBeginning Correlation functions calculations".format(t1-t0))
+    dtype = allowed_types[vector_type]
+    dtype = np.float32
+    ### Check if pandas is available - much faster to read in the data through pandas
+    t0=time.time()
+    print("Reading in the data...")
+    try:
+        if pd is not None:
+            df  = pd.read_csv(file,header=None,engine="c",dtype={"x":dtype,"y":dtype,"z":dtype},delim_whitespace=True)
+            ra  = np.asarray(df[0],dtype=dtype)
+            dec = np.asarray(df[1],dtype=dtype)
+            cz  = np.asarray(df[2],dtype=dtype)
+        else:
+            ra,dec,cz = np.genfromtxt(file,dtype=dtype,unpack=True)
+    except:
+        pass
 
 
-nthreads=4
-pimax=40.0
-binfile=os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_mocks/tests/","bins")
-autocorr=1
-numbins_to_print=5
-cosmology=1
-
-print("RA min = {} max = {}".format(np.min(ra),np.max(ra)))
-print("DEC min = {} max = {}".format(np.min(dec),np.max(dec)))
-print("cz min = {} max = {}".format(np.min(cz),np.max(cz)))
-
-print("\nRunning 2-D correlation function xi(rp,pi)")
-results_DDrppi = _countpairs_mocks.countpairs_rp_pi_mocks(autocorr, cosmology,nthreads,pimax,binfile,ra,dec,cz,ra,dec,cz)
-print("\n#            ****** DD(rp,pi): first {} bins  *******      ".format(numbins_to_print))
-print("#      rmin        rmax       rpavg     pi_upper     npairs")
-print("###########################################################")
-for ibin in range(numbins_to_print):
-    items = results_DDrppi[ibin]
-    print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
-print("-----------------------------------------------------------")
+    t1=time.time()    
+    print("Done reading the data - time taken = {0:10.1f} seconds.\nBeginning Correlation functions calculations".format(t1-t0))
 
 
-print("\nRunning angular correlation function w(theta)")
-results_wtheta = _countpairs_mocks.countpairs_theta_mocks(autocorr, cosmology, nthreads, binfile, ra,dec, ra,dec)
-print("\n#         ******  wtheta: first {} bins  *******        ".format(numbins_to_print))
-print("#      thetamin        thetamax       thetaavg      npairs")
-print("##########################################################")
-for ibin in range(numbins_to_print):
-    items = results_wtheta[ibin]
-    print("{0:14.4f} {1:14.4f} {2:14.4f} {3:14d}".format(items[0],items[1],items[2],items[3]))
-print("-----------------------------------------------------------")
+    nthreads=4
+    pimax=40.0
+    binfile=os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_mocks/tests/","bins")
+    autocorr=1
+    numbins_to_print=5
+    cosmology=1
 
-print("Done with all MOCK correlation calculations.")
+    print("RA min = {} max = {}".format(np.min(ra),np.max(ra)))
+    print("DEC min = {} max = {}".format(np.min(dec),np.max(dec)))
+    print("cz min = {} max = {}".format(np.min(cz),np.max(cz)))
+
+    print("\nRunning 2-D correlation function xi(rp,pi)")
+    results_DDrppi = _countpairs_mocks.countpairs_rp_pi_mocks(autocorr, cosmology,nthreads,pimax,binfile,ra,dec,cz,ra,dec,cz)
+    print("\n#            ****** DD(rp,pi): first {} bins  *******      ".format(numbins_to_print))
+    print("#      rmin        rmax       rpavg     pi_upper     npairs")
+    print("###########################################################")
+    for ibin in range(numbins_to_print):
+        items = results_DDrppi[ibin]
+        print("{0:12.4f} {1:12.4f} {2:10.4f} {3:10.1f} {4:10d}".format(items[0],items[1],items[2],items[3],items[4]))
+    print("-----------------------------------------------------------")
 
 
-print("Beginning the VPF")
-## Max. sphere radius of 10 Mpc
-rmax=10.0
-## 10 bins..so counts in spheres of radius 1, 2, 3, 4...10 Mpc spheres
-nbin=10
-num_spheres=10000
-num_pN=6
-threshold_neighbors=1 ## does not matter since we already have the centers
-volume=0.0 ## does not matter since we already have the centers
-centers_file=os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_mocks/tests/data/","Mr19_centers_xyz_forVPF_rmax_10Mpc.txt")
-Nran=num_spheres ## set it so that the code runs the loop
-results_vpf = _countpairs_mocks.countspheres_vpf_mocks(rmax,nbin,num_spheres,num_pN,threshold_neighbors,centers_file,cosmology,ra,dec,cz,ra,dec,cz)
-print("Done with the VPF.")
-tend=time.time()
-print("Done with all the MOCK clustering calculations. Total time taken = {:0.2f} seconds.".format(tend-tstart))
-      
+    print("\nRunning angular correlation function w(theta)")
+    results_wtheta = _countpairs_mocks.countpairs_theta_mocks(autocorr, cosmology, nthreads, binfile, ra,dec, ra,dec)
+    print("\n#         ******  wtheta: first {} bins  *******        ".format(numbins_to_print))
+    print("#      thetamin        thetamax       thetaavg      npairs")
+    print("##########################################################")
+    for ibin in range(numbins_to_print):
+        items = results_wtheta[ibin]
+        print("{0:14.4f} {1:14.4f} {2:14.4f} {3:14d}".format(items[0],items[1],items[2],items[3]))
+    print("-----------------------------------------------------------")
+
+    print("Done with all MOCK correlation calculations.")
+
+
+    print("Beginning the VPF")
+    ## Max. sphere radius of 10 Mpc
+    rmax=10.0
+    ## 10 bins..so counts in spheres of radius 1, 2, 3, 4...10 Mpc spheres
+    nbin=10
+    num_spheres=10000
+    num_pN=6
+    threshold_neighbors=1 ## does not matter since we already have the centers
+    volume=0.0 ## does not matter since we already have the centers
+    centers_file=os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_mocks/tests/data/","Mr19_centers_xyz_forVPF_rmax_10Mpc.txt")
+    Nran=num_spheres ## set it so that the code runs the loop
+    results_vpf = _countpairs_mocks.countspheres_vpf_mocks(rmax,nbin,num_spheres,num_pN,threshold_neighbors,centers_file,cosmology,ra,dec,cz,ra,dec,cz)
+    print("\n#            ******    pN: first {} bins  *******         ".format(numbins_to_print))
+    print('#       r    ',end="")
+
+    for ipn in range(num_pN):
+        print('        p{:0d}      '.format(ipn),end="")
+
+    print("")
+
+    print("###########",end="")
+    for ipn in range(num_pN):
+        print('################',end="")
+    print("")
+
+
+    for ibin in range(numbins_to_print):
+        items = results_vpf[ibin]
+        print('{0:10.2f} '.format(items[0]),end="")
+        for ipn in range(num_pN):
+            print(' {0:15.4e}'.format(items[ipn+1]),end="")
+        print("")
+
+    print("-----------------------------------------------------------")
+    print("Done with the VPF.")
+    tend=time.time()
+    print("Done with all the MOCK clustering calculations. Total time taken = {:0.2f} seconds.".format(tend-tstart))
+
+
+if __name__ == "__main__":
+    main()
+
