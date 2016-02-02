268d63c9fe8482efb7731b0e4f387f2a2aa46a58
Manodeep Sinha <manodeep@gmail.com>
2016-02-02 17:34:30 +1100

    Replaced bitbucket with the correct github

--- utils/utils.c
+++ utils/utils.c
@@ -3,7 +3,7 @@
 		This file is a part of the Corrfunc package
 		Copyright (C) 2015-- Manodeep Sinha (manodeep@gmail.com)
 		License: MIT LICENSE. See LICENSE file under the top-level
-		directory at https://bitbucket.org/manodeep/corrfunc/
+		directory at https://github.com/manodeep/Corrfunc/
 */
 
 /*
