443a05799913ead35a73048998eb5b837301ad2c
Manodeep Sinha <manodeep@gmail.com>
2016-02-13 14:09:43 +1100

    PEP8 changes to python code. Bumped version number, indicating a patch release.

--- Corrfunc/utils.py
+++ Corrfunc/utils.py
@@ -4,9 +4,9 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from future.utils import bytes_to_native_str
 import sys
-import os
+import os.path as path
 
-__all__ = ['rd','read_catalog']
+__all__ = ['rd', 'read_catalog']
 
 if sys.version_info[0] >= 3:
     def rd(filename):
@@ -15,7 +15,7 @@ if sys.version_info[0] >= 3:
         """
         with open(filename, encoding="utf-8") as f:
             r = f.read()
-            
+
         return r
 else:
     def rd(filename):
@@ -24,7 +24,7 @@ else:
         """
         with open(filename) as f:
             r = f.read()
-            
+
         return r
 
 
@@ -48,12 +48,13 @@ def read_catalog(filebase=None):
     import re
     import numpy as np
 
-    def read_ascii(filename,return_dtype=None):
+    def read_ascii(filename, return_dtype=None):
         if return_dtype is None:
-            raise ValueError("Return data-type must be set and a valid numpy data-type")
-        
-        ### check if pandas is available - much faster to read in the data through pandas
-        t0=time.time()
+            msg = 'Return data-type must be set and a valid numpy data-type'
+            raise ValueError(msg)
+
+        # check if pandas is available - much faster to read in the data
+        # using pandas
         print("Reading in the data...")
         try:
             import pandas as pd
@@ -61,99 +62,125 @@ def read_catalog(filebase=None):
             pd = None
 
         if pd is not None:
-            df = pd.read_csv(file,header=None,engine="c",dtype={"x":return_dtype,"y":return_dtype,"z":return_dtype},delim_whitespace=True)
-            x = np.asarray(df[0],dtype=return_dtype)
-            y = np.asarray(df[1],dtype=return_dtype)
-            z = np.asarray(df[2],dtype=return_dtype)
+            df = pd.read_csv(file, header=None,
+                             engine="c",
+                             dtype={"x": return_dtype,
+                                    "y": return_dtype,
+                                    "z": return_dtype},
+                             delim_whitespace=True)
+            x = np.asarray(df[0], dtype=return_dtype)
+            y = np.asarray(df[1], dtype=return_dtype)
+            z = np.asarray(df[2], dtype=return_dtype)
 
         else:
-            x,y,z = np.genfromtxt(file,dtype=return_dtype,unpack=True)
+            x, y, z = np.genfromtxt(file, dtype=return_dtype, unpack=True)
 
         return x, y, z
-    
-    def read_fastfood(filename,return_dtype=None):
+
+    def read_fastfood(filename, return_dtype=None, need_header=None):
         if return_dtype is None:
-            raise ValueError("Return data-type must be set and a valid numpy data-type")
+            msg = "Return data-type must be set and a valid numpy data-type"
+            raise ValueError(msg)
 
         import struct
         with open(filename, "rb") as f:
-            skip1 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
-            idat  = struct.unpack(bytes_to_native_str(b'@iiiii'),f.read(20))[0:5]
-            skip2 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
-            assert skip1  == 20 and skip2 == 20,"fast-food file seems to be incorrect (reading idat)"
+            skip1 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
+            idat = struct.unpack(bytes_to_native_str(b'@iiiii'),
+                                 f.read(20))[0:5]
+            skip2 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
+            assert skip1 == 20 and skip2 == 20,\
+                "fast-food file seems to be incorrect (reading idat)"
             ngal = idat[1]
-            ## now read fdat
-            skip1 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
-            fdat  = struct.unpack(bytes_to_native_str(b'@fffffffff'),f.read(36))[0:9]
-            skip2 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
-            assert skip1  == 36 and skip2 == 36,"fast-food file seems to be incorrect (reading fdat )"
 
-            skip1 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
-            znow  = struct.unpack(bytes_to_native_str(b'@f'),f.read(4))[0]
-            skip2 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
-            assert skip1  == 4 and skip2 == 4,"fast-food file seems to be incorrect (reading redshift)"
+            if need_header is not None:
+                # now read fdat
+                skip1 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
+                fdat = struct.unpack(bytes_to_native_str(b'@fffffffff'),
+                                     f.read(36))[0:9]
+                skip2 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
+                assert skip1 == 36 and skip2 == 36,\
+                    "fast-food file seems to be incorrect (reading fdat )"
 
-            ## read the padding bytes for the x-positions
-            skip1 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
-            assert skip1 == ngal*4 or skip1 == ngal*8, "fast-food file seems to be corrupt (padding bytes)"
+                skip1 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
+                znow = struct.unpack(bytes_to_native_str(b'@f'), f.read(4))[0]
+                skip2 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
+                assert skip1 == 4 and skip2 == 4,\
+                    "fast-food file seems to be incorrect (reading redshift)"
+            else:
+                fdat_bytes = 4 + 36 + 4
+                znow_bytes = 4 + 4 + 4
+                # seek over the fdat + znow fields + padding bytes
+                # from current position
+                f.seek(fdat_bytes + znow_bytes, 1)
 
-            ### seek back 4 bytes from current position
+            # read the padding bytes for the x-positions
+            skip1 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
+            assert skip1 == ngal * 4 or skip1 == ngal * 8, \
+                "fast-food file seems to be corrupt (padding bytes)"
+
+            # seek back 4 bytes from current position
             f.seek(-4, 1)
             pos = {}
-            for field in u'xyz':
-                skip1 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
-                assert skip1 == ngal*4 or skip1 == ngal*8, "fast-food file seems to be corrupt (padding bytes a)"
-                input_dtype = np.float32 if skip1/ngal == 4 else np.float
+            for field in 'xyz':
+                skip1 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
+                assert skip1 == ngal * 4 or skip1 == ngal * 8, \
+                    "fast-food file seems to be corrupt (padding bytes a)"
+                # the next division must be the integer division
+                input_dtype = np.float32 if skip1 // ngal == 4 else np.float
                 array = np.fromfile(f, input_dtype, ngal)
-                skip2 = struct.unpack(bytes_to_native_str(b'@i'),f.read(4))[0]
+                skip2 = struct.unpack(bytes_to_native_str(b'@i'), f.read(4))[0]
                 pos[field] = array if dtype is None else dtype(array)
 
-        x = pos[u'x']
-        y = pos[u'y']
-        z = pos[u'z']
+        x = pos['x']
+        y = pos['y']
+        z = pos['z']
 
-        return x,y,z
+        if need_header is not None:
+            return idat, fdat, znow, x, y, z
+        else:
+            return x, y, z
 
-    
     if filebase is None:
-        filename = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../xi_theory/tests/data/","gals_Mr19")
-        ## Figure out the datatype, use the header file in the include directory
-        ## because that is most likely correct (common.mk might have been modified
-        ## but not recompiled)
-        include_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                    "../include/", "countpairs.h")
+        filename = path.join(path.dirname(path.abspath(__file__)),
+                             "../xi_theory/tests/data/", "gals_Mr19")
+        # Figure out the datatype, use the header file in the include directory
+        # because that is most likely correct (common.mk might have been
+        # modified but not recompiled)
+        include_file = path.join(path.dirname(path.abspath(__file__)),
+                                 "../include/", "countpairs.h")
         includes = rd(include_file)
-        vector_type = re.search(r'(\w+)\s*\*\s*rupp\s*\;', includes, re.I).group(1)
-        allowed_types = {"float":np.float32,"double":np.float}
+        vector_type = re.search(r'(\w+)\s*\*\s*rupp\s*\;', includes,
+                                re.I).group(1)
+        allowed_types = {"float": np.float32, "double": np.float}
         if vector_type not in list(allowed_types.keys()):
-            print("Error: Unknown precision={0} found in header file {1}. Allowed types are `{2}'".format(vector_type,include_file,allowed_types))
+            print("Error: Unknown precision={0} found in header file {1}. \
+            Allowed types are `{2}'".format(vector_type,
+                                            include_file,
+                                            allowed_types))
             sys.exit()
-            
+
         dtype = allowed_types[vector_type]
-        allowed_exts = {'.ff' :read_fastfood,
-                        '.txt':read_ascii,
-                        '.dat':read_ascii,
-                        '.csv':read_ascii
+        allowed_exts = {'.ff': read_fastfood,
+                        '.txt': read_ascii,
+                        '.dat': read_ascii,
+                        '.csv': read_ascii
                         }
-        
 
         for e in allowed_exts:
-            if os.path.exists(filename+e):
+            if path.exists(filename + e):
                 f = allowed_exts[e]
-                x,y,z = f(filename+e, dtype)
-                return x,y,z
-        raise IOError("Could not locate {0} with any of these extensions = {1}".format(filename, allowed_exts.keys()))
+                x, y, z = f(filename + e, dtype)
+                return x, y, z
+        raise IOError("Could not locate {0} with any of these extensions \
+        = {1}".format(filename, allowed_exts.keys()))
     else:
-        ### Likely an user-supplied value
-        if os.path.exists(filebase):
-            extension = os.path.splitext(filebase)[1]
-            f = read_fastfood if u'.ff' in extension else read_ascii
+        # Likely an user-supplied value
+        if path.exists(filebase):
+            extension = path.splitext(filebase)[1]
+            f = read_fastfood if '.ff' in extension else read_ascii
 
-            ### default return is double
-            x,y,z = f(filebase, np.float)
-            return x,y,z
-        
-        raise IOError("Could not locate file {0}",filebase)
+            # default return is double
+            x, y, z = f(filebase, np.float)
+            return x, y, z
 
-        
-            
+        raise IOError("Could not locate file {0}", filebase)
--- setup.py
+++ setup.py
@@ -4,41 +4,48 @@
 from __future__ import (division, print_function)
 
 import os
+import os.path as path
 import fnmatch
 import sys
+from sys import version_info
 import re
 
 import Corrfunc
 from Corrfunc import rd
 
-## Make sure we are running on posix (Linux, Unix, MAC OSX)    
+# Make sure we are running on posix (Linux, Unix, MAC OSX)
 if os.name != 'posix':
     sys.exit("Sorry, Windows is not supported")
 
 
-common = rd(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                    "common.mk"))
+common = rd(path.join(path.dirname(path.abspath(__file__)),
+                      "common.mk"))
 name = re.search(r'DISTNAME\s*:*=\s*(\w+)', common).group(1)
-major = re.search(r'MAJOR\s*:*=\s*(\d)',common).group(1)
-minor = re.search(r'MINOR\s*:*=\s*(\d)',common).group(1)
-patch = re.search(r'PATCHLEVEL\s*:*=\s*(\d)',common).group(1)
-version = "{0}.{1}.{2}".format(major,minor,patch)
-## Check that version matches
+major = re.search(r'MAJOR\s*:*=\s*(\d)', common).group(1)
+minor = re.search(r'MINOR\s*:*=\s*(\d)', common).group(1)
+patch = re.search(r'PATCHLEVEL\s*:*=\s*(\d)', common).group(1)
+version = "{0}.{1}.{2}".format(major, minor, patch)
+# Check that version matches
 if Corrfunc.__version__ != version:
-    sys.exit("ERROR: Version mis-match. Python version found = {0} while C version claims {1}\n".format(Corrfunc.__version__, version))
+    msg = "ERROR: Version mis-match. Python version found = {0} \
+    while C version claims {1}".format(Corrfunc.__version__, version)
+    sys.exit(msg)
 
-min_py_major = int(re.search(r'MIN_PYTHON_MAJOR\s*:=\s*(\d)',common).group(1))
-min_py_minor = int(re.search(r'MIN_PYTHON_MINOR\s*:=\s*(\d)',common).group(1))
+min_py_major = int(re.search(r'MIN_PYTHON_MAJOR\s*:=\s*(\d)', common).group(1))
+min_py_minor = int(re.search(r'MIN_PYTHON_MINOR\s*:=\s*(\d)', common).group(1))
 
-min_numpy_major = int(re.search(r'MIN_NUMPY_MAJOR\s*:=\s*(\d)',common).group(1))
-min_numpy_minor = int(re.search(r'MIN_NUMPY_MINOR\s*:=\s*(\d)',common).group(1))
-   
-## Enforce minimum python version
-if sys.version_info[0] < min_py_major or (sys.version_info[0] == min_py_major  and sys.version_info[1] < min_py_minor):
-    raise RuntimeError('Sorry. Found python {0}.{1} but minimum required python version is {2}.{3}'.format(sys.version_info[0],sys.version_info[1],min_py_major,min_py_minor))
+min_np_major = int(re.search(r'MIN_NUMPY_MAJOR\s*:=\s*(\d)', common).group(1))
+min_np_minor = int(re.search(r'MIN_NUMPY_MINOR\s*:=\s*(\d)', common).group(1))
 
-## numpy 1.7 supports python 2.4-2.5; python 3.1-3.3. 
+# Enforce minimum python version
+if version_info[0] < min_py_major or \
+   (version_info[0] == min_py_major and version_info[1] < min_py_minor):
+    raise RuntimeError('Sorry. Found python {0}.{1} but minimum required \
+    python version is {2}.{3}'.format(version_info[0],
+                                      version_info[1],
+                                      min_py_major, min_py_minor))
 
+# numpy 1.7 supports python 2.4-2.5; python 3.1-3.3.
 try:
     from setuptools import setup, Extension
     from setuptools.command.build_ext import build_ext
@@ -53,133 +60,156 @@ if sys.argv[-1] == "publish":
 
 def run_command(command):
     import subprocess
-    # print("about to execute command `{0}`. sources = {1}".format(command,sources))
+    # print("about to execute command `{0}`. sources = {1}"
+    # .format(command, sources))
     proc = subprocess.Popen(command, stderr=subprocess.STDOUT, shell=True)
     output, stderr = proc.communicate(input)
     status = proc.wait()
     if status:
-        raise Exception("command = {0} failed with status {1:d}".format(command,status),
-                        output, status)
+        raise Exception("command = {0} failed with output = {1} status {2:d}\n"
+                        .format(command, output, status))
 
 
-class build_ext_subclass( build_ext ):
+class BuildExtSubclass(build_ext):
     def build_extensions(self):
-        ### Everything has already been configured within Make - so just call make
+        # Everything has already been configured - so just call make
         for ext in self.extensions:
             sources = ext.sources
-            
-            ## Blatantly copied from the Android distutils setup.py
-            ## But then modified to make it python3 compatible! 
+
+            # Blatantly copied from the Android distutils setup.py
+            # But then modified to make it python3 compatible!
             if sources is None:
                 raise Exception("in 'ext_modules' option (extension '%s')," +
-                     "'sources must be present and must be '" + 
-                     "a list of source filename") % ext.name
-            
-            if len(sources)==1:
-                ext_dir = os.path.dirname(sources[0])
+                                "'sources must be present and must be '" +
+                                "a list of source filename") % ext.name
+
+            if len(sources) == 1:
+                ext_dir = path.dirname(sources[0])
             else:
-                ### not debugged - likely to be wrong
-                ext_dir = os.path.commonprefix(sources)
-                
+                # not debugged - likely to be wrong
+                ext_dir = path.commonprefix(sources)
+
             command = "cd {0} && make ".format(ext_dir)
             run_command(command)
-            
+
             import shutil
             import errno
             try:
-                os.makedirs(os.path.dirname(self.get_ext_fullpath(ext.name)))
+                os.makedirs(path.dirname(self.get_ext_fullpath(ext.name)))
             except OSError as exception:
                 if exception.errno != errno.EEXIST:
                     raise
 
-            # full_name='{0}.so.{1}'.format(os.path.join(ext_dir,ext.name),version)
-            # full_build_name='{0}.{1}'.format(self.get_ext_fullpath(ext.name),version)
+            # full_name = '{0}.so.{1}'.format(path.join(ext_dir, ext.name),
+            #                                 version)
+            full_build_name = '{0}.{1}'.format(
+                self.get_ext_fullpath(ext.name), version)
 
-            full_name='{0}.so'.format(os.path.join(ext_dir,ext.name))
-            full_build_name='{0}'.format(self.get_ext_fullpath(ext.name))
+            full_name = '{0}.so'.format(path.join(ext_dir, ext.name))
+            full_build_name = '{0}'.format(self.get_ext_fullpath(ext.name))
 
             shutil.copyfile(full_name, full_build_name)
-            shutil.copyfile('{0}.so'.format(os.path.join(ext_dir,ext.name)),self.get_ext_fullpath(ext.name))
-            # print("name = {0} source = {1} dest = {2}".format(ext.name, full_name, full_build_name))
-            #os.symlink(full_build_name,self.get_ext_fullpath(ext.name))
+            # print("name = {0} source = {1} dest = {2}"
+            #       .format(ext.name, full_name, full_build_name))
+            # os.symlink(full_build_name, self.get_ext_fullpath(ext.name))
 
 
-def generate_extensions(python_dirs):            
+def generate_extensions(python_dirs):
     extensions = []
     for pdir in python_dirs:
-        mk = rd(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                    pdir, "Makefile"))
-        project   = re.search(r'PROJECT\s*:*=\s*(\w+)', mk).group(1)
+        mk = rd(path.join(path.dirname(path.abspath(__file__)),
+                          pdir, "Makefile"))
+        project = re.search(r'PROJECT\s*:*=\s*(\w+)', mk).group(1)
         src_files = re.findall(r'SOURCES\s*:*=\s*(\w+\.c)', mk)
 
-        sources = [os.path.join(pdir,f) for f in src_files]
-        ## print("Found project = {0} in dir = {1} with sources = {2}".format(project,pdir,sources))
+        sources = [path.join(pdir, f) for f in src_files]
+        # print("Found project = {0} in dir = {1} with sources = {2}".format(
+        #     project, pdir, sources))
         ext = Extension("{0}".format(project),
                         sources=sources,
                         )
-    
+
         extensions.append(ext)
 
     return extensions
 
-### Only python >= 3.5 supports the recursive glob, hence
-### defining the function that works on all reasonable pythons
-### http://stackoverflow.com/questions/2186525/use-a-glob-to-find-files-recursively-in-python
+# Only python >= 3.5 supports the recursive glob, hence
+# defining the function that works on all reasonable pythons
+# http://stackoverflow.com/questions/2186525/use-a-glob-to-
+# find-files-recursively-in-python
+
+
 def recursive_glob(rootdir='.', patterns=['*']):
-    return [os.path.join(looproot, filename)
+    return [path.join(looproot, filename)
             for looproot, _, filenames in os.walk(rootdir)
-            for filename in filenames for p in patterns 
+            for filename in filenames for p in patterns
             if fnmatch.fnmatch(filename, p)]
 
-### Taken from numpy setup.py    
+
+# Taken from numpy setup.py
 def setup_packages():
 
-    ### protect the user in case they run python setup.py not from root directory
-    src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    # protect the user in case they run python setup.py not from root directory
+    src_path = path.dirname(path.abspath(sys.argv[0]))
     old_path = os.getcwd()
     os.chdir(src_path)
     sys.path.insert(0, src_path)
-    
-    
-    ### create a list of the python extensions 
+
+    # create a list of the python extensions
     python_dirs = ["xi_theory/python_bindings",
                    "xi_mocks/python_bindings"]
     extensions = generate_extensions(python_dirs)
 
-    ### only run this if not creating source dist
+    # only run this if not creating source dist
     if "sdist" not in sys.argv:
         command = "make install"
         run_command(command)
 
-    ### find all the data-files required.
-    ### Now the lib + associated header files have been generated
-    ### and put in lib/ and include/
-    ### This step must run after ``make install``
-    dirs_patterns = {'xi_theory/tests/data': ['*.ff','*.txt','*.txt.gz','*.dat'],
-                     'xi_mocks/tests/data' : ['*.ff','*.txt','*.txt.gz','*.dat'],
-                     'xi_theory/tests'     : ['Mr19*','bins*','cmass*'],
-                     'xi_mocks/tests'      : ['Mr19*','bins*','angular_bins*'],
-                     'include':['count*.h'],
-                     'lib':['libcount*.a']
+    # find all the data-files required.
+    # Now the lib + associated header files have been generated
+    # and put in lib/ and include/
+    # This step must run after ``make install``
+    dirs_patterns = {'xi_theory/tests/data': ['*.ff', '*.txt',
+                                              '*.txt.gz', '*.dat'],
+                     'xi_mocks/tests/data': ['*.ff', '*.txt',
+                                             '*.txt.gz', '*.dat'],
+                     'xi_theory/tests': ['Mr19*', 'bins*', 'cmass*'],
+                     'xi_mocks/tests': ['Mr19*', 'bins*', 'angular_bins*'],
+                     'include': ['count*.h'],
+                     'lib': ['libcount*.a']
                      }
     data_files = []
     for d in dirs_patterns:
         patterns = dirs_patterns[d]
-        f = recursive_glob(d,patterns)
+        f = recursive_glob(d, patterns)
         data_files.extend(f)
 
-    ### change them to be relative to package dir rather than root
+    # change them to be relative to package dir rather than root
     data_files = ["../{0}".format(d) for d in data_files]
 
-    ## Fix long description for PyPI
+    # Fix long description for PyPI
     try:
         import pypandoc
         long_description = pypandoc.convert('README.md', 'rst')
     except(IOError, ImportError):
         long_description = rd('README.md')
-    
-    ### All book-keeping is done. 
+
+    # All book-keeping is done.
     base_url = "https://github.com/manodeep/Corrfunc"
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX',
+                   'Programming Language :: C',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5']
     metadata = dict(
         name=name,
         version=version,
@@ -188,41 +218,28 @@ def setup_packages():
         maintainer='Manodeep Sinha',
         maintainer_email='manodeep@gmail.com',
         url=base_url,
-        download_url='{0}/archive/{1}-{2}.tar.gz'.format(base_url,name,version),
+        download_url='{0}/archive/{1}-{2}.tar.gz'.format(
+            base_url, name, version),
         description='Blazing fast correlation functions on the CPU',
         long_description=long_description,
-        classifiers = [
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            "Intended Audience :: Science/Research",
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Operating System :: POSIX',
-            'Programming Language :: C',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            ],
+        classifiers=classifiers,
         license='MIT',
-        ### Solaris might work, Windows will almost certainly not work
-        platforms = [ "Linux", "Mac OSX", "Unix"],
-        keywords=['correlation functions','simulations','surveys','galaxies'],
+        # Solaris might work, Windows will almost certainly not work
+        platforms=["Linux", "Mac OSX", "Unix"],
+        keywords=['correlation functions', 'simulations',
+                  'surveys', 'galaxies'],
         packages=[name],
         ext_package=name,
         ext_modules=extensions,
-        package_data={'':data_files},
+        package_data={'': data_files},
         include_package_data=True,
-        install_requires=['setuptools','numpy>={0}.{1}'.format(min_numpy_major,min_numpy_minor),'future'],
+        install_requires=['setuptools',
+                          'numpy>={0}.{1}'.format(min_np_major, min_np_minor),
+                          'future'],
         zip_safe=False,
-        cmdclass = {'build_ext': build_ext_subclass},
-        )
+        cmdclass={'build_ext': BuildExtSubclass})
 
-
-    ### Now the actual setup
+    # Now the actual setup
     try:
         setup(**metadata)
     finally:
