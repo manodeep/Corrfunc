abeb0728147f46e32dd6ff77dbb270a71c429bbf
Manodeep Sinha <manodeep@gmail.com>
2016-04-11 08:28:16 +1000

    Restructured countpairs_wp.c -- going for a more modular kernel approach

---
+++ xi_theory/wp/wp_kernels.h
@@ -0,0 +1,60 @@
+/* File: wp_kernels.h */
+/*
+  This file is a part of the Corrfunc package
+  Copyright (C) 2015-- Manodeep Sinha (manodeep@gmail.com)
+  License: MIT LICENSE. See LICENSE file under the top-level
+  directory at https://github.com/manodeep/Corrfunc/
+*/
+
+#pragma once
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "function_precision.h"
+#include <inttypes.h>
+
+#if defined(USE_AVX) && defined(__AVX__)
+#include "avx_calls.h"
+#endif
+
+void same_cell_wp_kernel(const cellarray_index * first,
+                         DOUBLE * restrict const X,DOUBLE * restrict const Y, DOUBLE * restrict const Z, 
+                         const DOUBLE sqr_rpmax, const DOUBLE sqr_rpmin, const int nbin, const DOUBLE rupp_sqr[] , const DOUBLE pimax
+#ifdef USE_AVX
+                         ,const AVX_FLOATS m_rupp_sqr[] 
+#ifdef OUTPUT_RPAVG
+                         ,const AVX_FLOATS m_kbin[]
+#endif                                            
+#endif                                            
+                                       
+                                       
+#ifdef OUTPUT_RPAVG
+                         ,DOUBLE src_rpavg[]
+#endif                         
+                         ,uint64_t *src_npairs);
+
+
+    
+void different_cell_wp_kernel(const cellarray_index * first, const cellarray_index *second,
+                              DOUBLE * restrict const X,DOUBLE * restrict const Y,DOUBLE * restrict const Z,
+                              const DOUBLE sqr_rpmax, const DOUBLE sqr_rpmin, const int nbin, const DOUBLE rupp_sqr[], const DOUBLE pimax,
+                              const DOUBLE off_xwrap, const DOUBLE off_ywrap, const DOUBLE off_zwrap
+#ifdef USE_AVX
+                              ,const AVX_FLOATS m_rupp_sqr[] 
+#ifdef OUTPUT_RPAVG
+                              ,const AVX_FLOATS m_kbin[]
+#endif                                            
+#endif                                            
+                                            
+                                            
+#ifdef OUTPUT_RPAVG
+                              ,DOUBLE src_rpavg[]
+#endif                         
+                              ,uint64_t *src_npairs);
+    
+#ifdef __cplusplus
+}
+#endif
