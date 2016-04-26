524dbe647a2c6102e1af65d2dfd8269e0f72d307
Manodeep Sinha <manodeep@gmail.com>
2016-04-27 07:08:50 +1000

    Began adding runtime dispatch for wp. Moved the fallback code into the kernels file as a function

--- xi_theory/wp/wp_driver.c
+++ xi_theory/wp/wp_driver.c
@@ -30,134 +30,66 @@ void wp_driver(DOUBLE *x0, DOUBLE *y0, DOUBLE *z0, const int64_t N0,
 #endif                         
                ,uint64_t *src_npairs)
 {
-
-#if defined(USE_AVX) && defined(__AVX__)
-    /*----------------- AVX --------------------*/
-    AVX_FLOATS m_rupp_sqr[nbin];
-    for(int i=0;i<nbin;i++) {
-        m_rupp_sqr[i] = AVX_SET_FLOAT(rupp_sqr[i]);
-    }
+    const int unroll_fac=4;
+    if(N1 < unroll_fac*NVEC) {
+        wp_fallback(x0, y0, z0, N0,
+                    x1, y1, z1, N1, same_cell,
+                    sqr_rpmax, sqr_rpmin,  nbin, rupp_sqr, pimax,
+                    off_xwrap, off_ywrap, off_zwrap
 #ifdef OUTPUT_RPAVG
-    AVX_FLOATS m_kbin[nbin];
-    for(int i=0;i<nbin;i++) {
-        m_kbin[i] = AVX_SET_FLOAT((DOUBLE) i);
-    }
-#endif//RPAVG + AVX
-
-    //AVX is available and wanted -> call the AVX function
-    wp_avx_intrinsics(x0, y0, z0, N0,
-                      x1, y1, z1, N1, same_cell,
-                      sqr_rpmax, sqr_rpmin,  nbin, rupp_sqr, pimax,
-                      off_xwrap, off_ywrap, off_zwrap
-                      ,m_rupp_sqr
-#ifdef OUTPUT_RPAVG
-                      ,m_kbin
-                      ,src_rpavg
+                    ,src_rpavg
 #endif                         
-                      ,src_npairs);
-    
-    //Already dispatched to appropriate AVX function -> return;
-    return;
-    /*----------------- END OF AVX --------------------*/
-#else //AVX
-
-#if defined (__SSE4_2__)
-
-    /*------------------ SSE -------------------*/
-    SSE_FLOATS m_rupp_sqr[nbin];
-    for(int i=0;i<nbin;i++) {
-        m_rupp_sqr[i] = SSE_SET_FLOAT(rupp_sqr[i]);
+                    ,src_npairs);
+        return;
     }
+
+
+	//Seriously this is the declaration for the function pointers...here be dragons.
+	void (*allfunctions[]) ( DOUBLE *x0, DOUBLE *y0, DOUBLE *z0, const int64_t N0,
+                             DOUBLE *x1, DOUBLE *y1, DOUBLE *z1, const int64_t N1, const int same_cell,
+                             const DOUBLE sqr_rpmax, const DOUBLE sqr_rpmin, const int nbin, const DOUBLE *rupp_sqr, const DOUBLE pimax,
+                             const DOUBLE off_xwrap, const DOUBLE off_ywrap, const DOUBLE off_zwrap
 #ifdef OUTPUT_RPAVG
-    SSE_FLOATS m_kbin[nbin];
-    for(int i=0;i<nbin;i++) {
-        m_kbin[i] = SSE_SET_FLOAT((DOUBLE) i);
-    }
-#endif//RPAVG + SSE
-    wp_sse_intrinsics(x0, y0, z0, N0,
-                      x1, y1, z1, N1, same_cell, 
-                      sqr_rpmax, sqr_rpmin,  nbin, rupp_sqr, pimax,
-                      off_xwrap, off_ywrap, off_zwrap
-                      ,m_rupp_sqr
-#ifdef OUTPUT_RPAVG
-                      ,m_kbin
-                      ,src_rpavg
+                             ,DOUBLE *src_rpavg
 #endif                         
-                      ,src_npairs);
-    //appropriate SSE function called -> return;
-    return;
-    /*----------------- END OF SSE --------------------*/
-#else
-
-    /*----------------- FALLBACK CODE --------------------*/
-    uint64_t npairs[nbin];
-    for(int i=0;i<nbin;i++) {
-        npairs[i]=0;
-    }
-#ifdef OUTPUT_RPAVG
-    DOUBLE rpavg[nbin];
-    for(int i=0;i<nbin;i++) {
-        rpavg[i]=0;
-    }
-#endif//OUTPUT_RPAVG
-
-    /* naive implementation that is guaranteed to compile */
-    for(int64_t i=0;i<N0;i++) {
-        const DOUBLE xpos = *x0++ + off_xwrap;
-        const DOUBLE ypos = *y0++ + off_ywrap;
-        const DOUBLE zpos = *z0++ + off_zwrap;
-
-        DOUBLE *localz1 = (DOUBLE *) z1; 
-        int64_t j=0;
-        if(same_cell == 1) {
-            j = i+1;
-            localz1 += j;
-        } else {
-            while(j < N1){
-                const DOUBLE dz = *localz1++ - zpos;
-                if(dz > -pimax) break;
-                j++;
-            }
-            localz1--;
-        }
-        DOUBLE *localx1 = x1 + j; 
-        DOUBLE *localy1 = y1 + j;
-
-        
-        for(;j<N1;j++) {
-            const DOUBLE dx = *localx1++ - xpos;
-            const DOUBLE dy = *localy1++ - ypos;
-            const DOUBLE dz = *localz1++ - zpos;
-            if(dz >=pimax) break;
-
-            const DOUBLE r2 = dx*dx + dy*dy;
-            if(r2 >= sqr_rpmax || r2 < sqr_rpmin) continue;
-#ifdef OUTPUT_RPAVG
-            const DOUBLE r = SQRT(r2);
+                             ,uint64_t *src_npairs)
+		= {
+#if defined(__AVX__) && defined(USE_AVX)		   
+        wp_avx_intrinsics,
+#endif			 
+#ifdef __SSE4_2__
+        wp_sse_intrinsics,
 #endif
-            for(int kbin=nbin-1;kbin>=1;kbin--){
-                if(r2 >= rupp_sqr[kbin-1]) {
-                    npairs[kbin]++;
+        wp_fallback
+	};
+    //the fastest available code will always be at index 0.
+    const int func_dispatch = 0;
+    
+/* #if (!(defined(__AVX__) && defined(USE_AVX))) || !defined(__SSE4_2__) */
+/*     //Old hardware probably -> no AVX or SSE -> use fallback */
+/*     const int func_dispatch = 0; // */
+/* #else     */
+/*     //At least AVX or SSE is available. */
+/* #if defined(__AVX__) && defined(USE_AVX) */
+/*     //call AVX for > 2*NVEC -> unroll AVX loop by 2 */
+/*     const int func_dispatch = 0; */
+/* #else */
+/* #if defined(__SSE4_2__) */
+/*     const int func_dispatch = 0; */
+/* #endif//SSE4.2     */
+/* #endif//__AVX__ */
+/* #endif//__AVX__ || SSE */
+
+    //Now call the appropriate function -> look, runtime dispatch !
+    (allfunctions[func_dispatch])(x0, y0, z0, N0,
+                                  x1, y1, z1, N1, same_cell,
+                                  sqr_rpmax, sqr_rpmin,  nbin, rupp_sqr, pimax,
+                                  off_xwrap, off_ywrap, off_zwrap
 #ifdef OUTPUT_RPAVG
-                    rpavg[kbin] += r;
-#endif                    
-                    break;
-                }
-            }//searching for kbin loop                                                               
-        }
-    }
-
-    for(int i=0;i<nbin;i++) {
-        src_npairs[i] += npairs[i];
-#ifdef OUTPUT_RPAVG
-        src_rpavg[i] += rpavg[i];
-#endif        
-    }
-    /*----------------- FALLBACK CODE --------------------*/
-
-#endif//SSE
-#endif//AVX
-
+                                  ,src_rpavg
+#endif                         
+                                  ,src_npairs);
+    
+    
 }
-
 
