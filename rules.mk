b8ae7d47590aebdc57c4657b2a26d3e460141423
Manodeep Sinha <manodeep@gmail.com>
2016-08-15 03:37:06 -0500

    Now forcing double/float into the header and C files.

--- rules.mk
+++ rules.mk
@@ -1,47 +1,57 @@
 TARGETOBJS := $(TARGETSRC:.c=.o)
 LIBOBJS :=$(LIBSRC:.c=.o)
 
-$(TARGET): $(TARGETOBJS) $(INCL) $(ROOT_DIR)/common.mk $(ROOT_DIR)/theory.options Makefile 
-	$(CC) $(TARGETOBJS) $(CLINK) $(GSL_LINK) -o $@
+$(TARGET): $(TARGETOBJS) $(ROOT_DIR)/common.mk $(ROOT_DIR)/theory.options Makefile 
+	$(CC) $(TARGETOBJS) $(CLINK) -o $@
 
 $(TARGET).o: $(TARGET).c $(ROOT_DIR)/common.mk Makefile $(INCL)
-	$(CC) $(OPT) $(CFLAGS) $(INCLUDE) $(GSL_CFLAGS) -c $< -o $@
+	$(CC) $(OPT) $(CFLAGS) $(INCLUDE) -c $< -o $@
 
 $(UTILS_DIR)/%.o: $(ROOT_DIR)/common.mk Makefile $(UTILS_DIR)/%.c
 	$(MAKE) -C $(UTILS_DIR)
 
-$(UTILS_DIR)/%.h: $(ROOT_DIR)/common.mk Makefile $(UTILS_DIR)/%.h.src
+$(UTILS_DIR)/%.h: $(UTILS_DIR)/%.h.src
 	$(MAKE) -C $(UTILS_DIR)
 
-$(IO_DIR)/%.h: $(ROOT_DIR)/common.mk Makefile $(IO_DIR)/%.h.src
+$(IO_DIR)/%.h: $(IO_DIR)/%.h.src
+	$(MAKE) -C $(IO_DIR)
+
+$(IO_DIR)/%.o: $(IO_DIR)/%.c $(ROOT_DIR)/common.mk Makefile
 	$(MAKE) -C $(IO_DIR)
 
 %_float.c: %.c.src Makefile
 	@$(ECHO_COMMAND) "/* This file is auto-generated from $(notdir $<) */" > $@
+	@$(ECHO_COMMAND) "#ifdef DOUBLE_PREC" >> $@
+	@$(ECHO_COMMAND) "#undef DOUBLE_PREC" >> $@
+	@$(ECHO_COMMAND) "#endif" >> $@
 	sed -e "s/DOUBLE/float/g" $< >> $@
 
 %_float.h: %.h.src Makefile
 	@$(ECHO_COMMAND) "/* This file is auto-generated from $(notdir $<) */" > $@
+	@$(ECHO_COMMAND) "#ifdef DOUBLE_PREC" >> $@
+	@$(ECHO_COMMAND) "#undef DOUBLE_PREC" >> $@
+	@$(ECHO_COMMAND) "#endif" >> $@
 	sed -e "s/DOUBLE/float/g"  $< >> $@
 
 %_double.c: %.c.src Makefile
 	@$(ECHO_COMMAND) "/* This file is auto-generated from $(notdir $<) */" > $@
+	@$(ECHO_COMMAND) "#ifndef DOUBLE_PREC" >> $@
+	@$(ECHO_COMMAND) "#define DOUBLE_PREC" >> $@
+	@$(ECHO_COMMAND) "#endif" >> $@
 	sed -e "s/DOUBLE/double/g"  $< >> $@
+
 %_double.h: %.h.src Makefile
 	@$(ECHO_COMMAND) "/* This file is auto-generated from $(notdir $<) */" > $@
+	@$(ECHO_COMMAND) "#ifndef DOUBLE_PREC" >> $@
+	@$(ECHO_COMMAND) "#define DOUBLE_PREC" >> $@
+	@$(ECHO_COMMAND) "#endif" >> $@
 	sed -e "s/DOUBLE/double/g"  $< >> $@
 
-%_double.o: %_double.c %_double.h $(INCL) $(ROOT_DIR)/theory.options $(ROOT_DIR)/common.mk Makefile
-	$(CC) -DDOUBLE_PREC $(CFLAGS) $(INCLUDE) -c $< -o $@
+%.o: %.c $(INCL) $(ROOT_DIR)/common.mk Makefile
+	@echo running default make rule
+	$(CC) $(CFLAGS) $(INCLUDE) -c $< -o $@
 
-%_float.o: %_float.c %_float.h $(INCL)
-	$(CC) -DNDOUBLE_PREC $(CFLAGS) $(INCLUDE) -c $< -o $@
-
-%.o: %.c %.h $(INCL) $(ROOT_DIR)/common.mk Makefile
-	$(CC) $(CFLAGS) $(INCLUDE) $(GSL_CFLAGS) -c $< -o $@
-
-
-$(LIBRARY): $(LIBOBJS) $(INCL) $(ROOT_DIR)/mocks.options $(ROOT_DIR)/theory.options $(ROOT_DIR)/common.mk Makefile 
+$(LIBRARY): $(LIBOBJS) $(ROOT_DIR)/mocks.options $(ROOT_DIR)/theory.options $(ROOT_DIR)/common.mk Makefile 
 	ar rcs $@ $(LIBOBJS)
 
 $(INSTALL_LIB_DIR)/%.a: %.a $(UTILS_DIR)/defs.h | $(INSTALL_LIB_DIR) $(INSTALL_HEADERS_DIR)
@@ -49,13 +59,13 @@ $(INSTALL_LIB_DIR)/%.a: %.a $(UTILS_DIR)/defs.h | $(INSTALL_LIB_DIR) $(INSTALL_H
 	sed -e "s/DOUBLE/$(VECTOR_TYPE)/g" $(LIBRARY_HEADERS) > $(INSTALL_HEADERS_DIR)/$(LIBRARY_HEADERS)
 	cp -p $(UTILS_DIR)/defs.h $(INSTALL_HEADERS_DIR)/
 
-
 $(INSTALL_BIN_DIR)/%: %
 	cp -p $< $(INSTALL_BIN_DIR)/
+
 $(INSTALL_LIB_DIR) $(INSTALL_BIN_DIR) $(INSTALL_HEADERS_DIR):
 	mkdir -p $@
 
-.PHONY: clean clena celan install lib tests distclean all realclean
+.PHONY: clean clena celan install lib tests distclean all realclean libs
 
 celna: clean
 celan: clean
