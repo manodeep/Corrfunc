7ecb592944a24d1bab4e40f6a27db702ad739b20
Manodeep Sinha <manodeep@gmail.com>
2016-08-24 06:48:50 +1000

    Fixed parallel builds for the theory routines

--- rules.mk
+++ rules.mk
@@ -6,20 +6,8 @@ LIBOBJS :=$(LIBSRC:.c=.o)
 $(TARGET): $(TARGETOBJS) $(ROOT_DIR)/common.mk 
 	$(CC) $(TARGETOBJS) $(C_LIBRARIES) $(CLINK) $(EXTRA_LINK) -o $@
 
-$(TARGET).o: $(TARGET).c $(ROOT_DIR)/common.mk Makefile $(INCL) $(ROOT_DIR)/theory.options $(ROOT_DIR)/mocks.options 
+$(TARGET).o: $(TARGET).c $(ROOT_DIR)/common.mk Makefile $(ROOT_DIR)/theory.options $(ROOT_DIR)/mocks.options 
 	$(CC) $(OPT) $(CFLAGS) $(INCLUDE) $(EXTRA_INCL) -c $< -o $@
-
-$(UTILS_DIR)/%.o: $(ROOT_DIR)/common.mk Makefile $(UTILS_DIR)/%.c
-	$(MAKE) -C $(UTILS_DIR)
-
-$(UTILS_DIR)/%.h: $(UTILS_DIR)/%.h.src
-	$(MAKE) -C $(UTILS_DIR)
-
-$(IO_DIR)/%.h: $(IO_DIR)/%.h.src
-	$(MAKE) -C $(IO_DIR)
-
-$(IO_DIR)/%.o: $(IO_DIR)/%.c $(ROOT_DIR)/common.mk Makefile
-	$(MAKE) -C $(IO_DIR)
 
 %_float.c: %.c.src Makefile
 	@echo "/* This file is auto-generated from $(notdir $<) */" > $@
@@ -55,16 +43,20 @@ $(IO_DIR)/%.o: $(IO_DIR)/%.c $(ROOT_DIR)/common.mk Makefile
 %_float.o: %_float.c %.c.src Makefile
 	$(CC) -DNDOUBLE_PREC $(CFLAGS) $(INCLUDE) $(EXTRA_INCL) -c $< -o $@
 
-%.o: %.c $(INCL) $(ROOT_DIR)/common.mk Makefile
+%.o: %.c $(ROOT_DIR)/common.mk Makefile
 	$(CC) $(CFLAGS) $(INCLUDE) $(EXTRA_INCL) -c $< -o $@
 
 $(LIBRARY): $(LIBOBJS) $(ROOT_DIR)/mocks.options $(ROOT_DIR)/theory.options $(ROOT_DIR)/common.mk Makefile 
 	ar rcs $@ $(LIBOBJS)
 
 
-$(INSTALL_LIB_DIR)/%.a: %.a $(UTILS_DIR)/defs.h | $(INSTALL_LIB_DIR) $(INSTALL_HEADERS_DIR)
+$(INSTALL_LIB_DIR)/%.a: %.a | $(INSTALL_LIB_DIR) 
 	cp -p $(LIBRARY) $(INSTALL_LIB_DIR)/
-	cp -p $(LIBRARY_HEADERS) $(INSTALL_HEADERS_DIR)/
+
+$(INSTALL_HEADERS_DIR)/%.h: %.h $(INSTALL_HEADERS_DIR)/defs.h | $(INSTALL_HEADERS_DIR)
+	cp -p $< $@
+
+$(INSTALL_HEADERS_DIR)/defs.h:$(UTILS_DIR)/defs.h | $(INSTALL_HEADERS_DIR)
 	cp -p $(UTILS_DIR)/defs.h $(INSTALL_HEADERS_DIR)/
 
 $(INSTALL_BIN_DIR)/%: %
