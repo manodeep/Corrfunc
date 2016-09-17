b140fcc23fd461f072e632b447b83711d52bbfb6
Manodeep Sinha <manodeep@gmail.com>
2016-09-18 08:00:40 +1000

    Not referenced anywhere currently but I might need cache linesizes in the future [ci skip]

---
+++ utils/cache.h
@@ -0,0 +1,22 @@
+/* File: cache.h */
+/*
+  This file is a part of the Corrfunc package
+  Copyright (C) 2015-- Manodeep Sinha (manodeep@gmail.com)
+  License: MIT LICENSE. See LICENSE file under the top-level
+  directory at https://github.com/manodeep/Corrfunc/
+*/
+
+#pragma once
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*Taken from http://stackoverflow.com/questions/794632/programmatically-get-the-cache-line-size*/
+#include <stddef.h>
+size_t cache_line_size(void);
+    
+#ifdef __cplusplus
+}
+#endif
