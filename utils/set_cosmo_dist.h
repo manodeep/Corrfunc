5d42eec4ffb0c824f402a599633870fff6cce730
Manodeep Sinha <manodeep@gmail.com>
2016-09-19 08:22:26 +1000

    Beginning the fixes for cosmological distance calculations in mocks. Fixed #85

--- utils/set_cosmo_dist.h
+++ utils/set_cosmo_dist.h
@@ -10,21 +10,15 @@
 #define _SET_COSMO_DIST_H_
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 
-#define COSMO_DIST_SIZE 10000
-#define MAX_REDSHIFT_FOR_COSMO_DIST     1.0
 #define SPEED_OF_LIGHT 299800.0
 
-
-
-    int set_cosmo_dist(const double zmax,const int max_size,double *zc,double *dc,const int lasdamas_cosmology);
+    extern int set_cosmo_dist(const double zmax,const int max_size,double *zc,double *dc,const int lasdamas_cosmology);
 
 #ifdef __cplusplus
 }
