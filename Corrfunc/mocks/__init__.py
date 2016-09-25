8dfee9da06023dba52457e64bb229db9b43c2fd1
Manodeep Sinha <manodeep@gmail.com>
2016-09-26 08:32:16 +1000

    Fixed imports. This way the theory/mocks functions can be imported directly from the theory/mocks package

--- Corrfunc/mocks/__init__.py
+++ Corrfunc/mocks/__init__.py
@@ -12,5 +12,10 @@ import sys
 __author__ = ('Manodeep Sinha')
 __all__ = ("DDrppi_mocks", "DDtheta_mocks", "vpf_mocks", )
 
+
+from .DDrppi_mocks import DDrppi_mocks
+from .DDtheta_mocks import DDtheta_mocks
+from .vpf_mocks import vpf_mocks
+
 if sys.version_info[0] < 3:
     __all__ = [n.encode('ascii') for n in __all__]
