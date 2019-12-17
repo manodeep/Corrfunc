8ecb1934170109e02284836f1b0a4cac64786dec
Manodeep Sinha <manodeep@gmail.com>
2019-12-17 13:21:35 +1100

    Fixed two long-standing issues with the docs.

--- docs/source/apidoc.sh
+++ docs/source/apidoc.sh
@@ -1,6 +1,26 @@
-# bash
+#! /bin/bash
 
 if ! python -c 'import numpydoc'; then easy_install --user numpydoc; fi
 if ! python -c 'import sphinx'; then easy_install --user sphinx; fi
 
-sphinx-apidoc -H "Comprehensive API reference" -M -f -o source/api/ ../Corrfunc/
+outdir=source/api
+sphinx-apidoc -H "Comprehensive API reference" -M -f -o $outdir ../Corrfunc/ ../Corrfunc/tests.py ../Corrfunc/call_correlation_functions.py ../Corrfunc/call_correlation_functions_mocks.py
+
+# Fix the blank sub-modules in the Corrfunc file
+for docfile in $outdir/Corrfunc.rst
+do
+    # Delete three lines following the "submodules"
+    sed -e '/Submodules/{N;N;d;}' $docfile > xx
+    mv xx  $docfile
+done
+
+
+# Fix the duplicate entries for the various pair-counters
+# (e.g., Corrfunc.theory.DD *and* Corrfunc.theory.DD.DD)
+for docfile in $outdir/Corrfunc.mocks.rst $outdir/Corrfunc.theory.rst
+do
+    # Delete ALL lines following this "submodule" line in the theory/mocks
+    # auto-generated documentation
+    sed  -n '/Submodules/q;p' $docfile > xx
+    mv xx  $docfile
+done
