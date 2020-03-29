8d303ab290dca57be652a9bb6ebde1bbbf87f75f
Kris Stern <krisastern@gobuddy.asia>
2020-03-29 18:59:08 +0800

    Make second batch of formatting changes

--- Corrfunc/mocks/DDtheta_mocks.py
+++ Corrfunc/mocks/DDtheta_mocks.py
@@ -294,8 +294,8 @@ def DDtheta_mocks(autocorr, nthreads, binfile,
 
     # Ensure all input arrays are native endian
     RA1, DEC1, weights1, RA2, DEC2, weights2 = [
-        convert_to_native_endian(arr, warn=True) for arr in
-        [RA1, DEC1, weights1, RA2, DEC2, weights2]]
+            convert_to_native_endian(arr, warn=True) for arr in
+            [RA1, DEC1, weights1, RA2, DEC2, weights2]]
 
     fix_ra_dec(RA1, DEC1)
     if autocorr == 0:
@@ -314,21 +314,20 @@ def DDtheta_mocks(autocorr, nthreads, binfile,
     integer_isa = translate_isa_string_to_enum(isa)
     rbinfile, delete_after_use = return_file_with_rbins(binfile)
     with sys_pipes():
-        extn_results = DDtheta_mocks_extn(
-            autocorr, nthreads, rbinfile,
-            RA1, DEC1,
-            verbose=verbose,
-            link_in_dec=link_in_dec,
-            link_in_ra=link_in_ra,
-            output_thetaavg=output_thetaavg,
-            fast_acos=fast_acos,
-            ra_refine_factor=ra_refine_factor,
-            dec_refine_factor=dec_refine_factor,
-            max_cells_per_dim=max_cells_per_dim,
-            copy_particles=copy_particles,
-            enable_min_sep_opt=enable_min_sep_opt,
-            c_api_timer=c_api_timer,
-            isa=integer_isa, **kwargs)
+        extn_results = DDtheta_mocks_extn(autocorr, nthreads, rbinfile,
+                                          RA1, DEC1,
+                                          verbose=verbose,
+                                          link_in_dec=link_in_dec,
+                                          link_in_ra=link_in_ra,
+                                          output_thetaavg=output_thetaavg,
+                                          fast_acos=fast_acos,
+                                          ra_refine_factor=ra_refine_factor,
+                                          dec_refine_factor=dec_refine_factor,
+                                          max_cells_per_dim=max_cells_per_dim,
+                                          copy_particles=copy_particles,
+                                          enable_min_sep_opt=enable_min_sep_opt,
+                                          c_api_timer=c_api_timer,
+                                          isa=integer_isa, **kwargs)
     if extn_results is None:
         msg = "RuntimeError occurred"
         raise RuntimeError(msg)
@@ -581,17 +580,16 @@ def find_fastest_DDtheta_mocks_bin_refs(autocorr, nthreads, binfile,
             total_sqr_runtime = 0.0
             for _ in range(nrepeats):
                 t0 = time.time()
-                extn_results = DDtheta_mocks_extn(
-                    autocorr, nthreads, rbinfile,
-                    RA1, DEC1, RA2, DEC2,
-                    link_in_dec=link_in_dec,
-                    link_in_ra=link_in_ra,
-                    verbose=verbose,
-                    output_thetaavg=output_thetaavg,
-                    ra_refine_factor=nRA,
-                    dec_refine_factor=nDEC,
-                    max_cells_per_dim=max_cells_per_dim,
-                    isa=integer_isa)
+                extn_results = DDtheta_mocks_extn(autocorr, nthreads, rbinfile,
+                                                  RA1, DEC1, RA2, DEC2,
+                                                  link_in_dec=link_in_dec,
+                                                  link_in_ra=link_in_ra,
+                                                  verbose=verbose,
+                                                  output_thetaavg=output_thetaavg,
+                                                  ra_refine_factor=nRA,
+                                                  dec_refine_factor=nDEC,
+                                                  max_cells_per_dim=max_cells_per_dim,
+                                                  isa=integer_isa)
                 t1 = time.time()
 
                 if extn_results is None:
