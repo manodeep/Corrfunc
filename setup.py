d05bcabec570ea7872c596f561457cb88aaaebb0
Chris Byrohl <9221545+cbyrohl@users.noreply.github.com>
2023-08-16 21:15:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -560,7 +560,7 @@ def setup_packages():
                                                       min_np_minor),
                               'future',
                               'wurlitzer'],
-            python_requires='>=2.7,!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*,, <4',
+            python_requires='>=2.7,!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, <4',
             zip_safe=False,
             cmdclass={'build_ext': BuildExtSubclass})
 
