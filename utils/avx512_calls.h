fbf4d08a6582530418b64eeefacc4beccdfb5c14
Manodeep Sinha <manodeep@gmail.com>
2026-08-17 09:49:42 +1000

    Removed `avx512_calls.c` by replacing LUTs with intrinsics / shifts (#357)
    * Removed avx512_calls.c from the Makefiles
    
    * Replaced the two lookup tables with a popcnt and a bit-shifting method to calculate load-mask (taken from simSIMD)
    
    * Removed the source and removed unused macros and function prototypes from the header
    
    * Fixed the few instances of masks_per that I overlooked during the whitespace fixing
    
    * Fixed typo
    
    * Forgot to remove avx512_calls.c from the utils/Makefile (but didn't get caught since utils/ Makefiles never get invoked)

--- utils/avx512_calls.h
+++ utils/avx512_calls.h
@@ -42,18 +42,6 @@ extern "C" {
 #define AVX512_MASK_BITWISE_NOT(X,Y)              _mm512_knot(X) //~X
 
     
-
-  /* For setting up the array that contains the number bits set in a mask */
-  // Can be used to generate up to 16 bit lookup tables
-#   define B2(n)  n,      n+1,      n+1,      n+2
-#   define B4(n)  B2(n),  B2(n+1),  B2(n+1),  B2(n+2)
-#   define B6(n)  B4(n),  B4(n+1),  B4(n+1),  B4(n+2)
-#   define B8(n)  B6(n),  B6(n+1),  B6(n+1),  B6(n+2)
-#   define B10(n) B8(n),  B8(n+1),  B8(n+1),  B8(n+2)
-#   define B12(n) B10(n), B10(n+1), B10(n+1), B10(n+2)
-#   define B14(n) B12(n), B12(n+1), B12(n+1), B12(n+2)
-#   define B16(n) B14(0),B14(1), B14(1),   B14(2)
-
 #ifndef DOUBLE_PREC
 
 #define DOUBLE                              float  
@@ -365,12 +353,6 @@ static inline AVX512_FLOATS inv_cosine_avx512(const AVX512_FLOATS X, const int o
   }
 
 #endif
-
-  extern const int64_t bits_set_in_avx512_mask_float[];
-  extern const uint16_t masks_per_misalignment_value_float[];
-
-  extern const int64_t bits_set_in_avx512_mask_double[];
-  extern const uint8_t masks_per_misalignment_value_double[];
 
   union int16 {
     AVX512_INTS m_ibin;
